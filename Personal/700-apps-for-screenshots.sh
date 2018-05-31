bb1cefd96de76a90d6454b92db31d0ea64eda498
Erik Dubois <erik.dubois@gmail.com>
2018-05-31 19:09:26 +0200

    update script

--- Personal/700-apps-for-screenshots.sh
+++ Personal/700-apps-for-screenshots.sh
@@ -13,20 +13,6 @@ set -e
 #
 ##################################################################################################################
 
-#!/bin/bash
-set -e
-##################################################################################################################
-# Author	:	Erik Dubois
-# Website	:	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
-##################################################################################################################
 # remember : toilet -f mono12 -F metal ArcoLinux
 
 sudo pacman -S asciiquarium --noconfirm --needed
