bb265a601edbcb36435ed1e93265400aa5cc4b3c
Erik Dubois <erik.dubois@gmail.com>
2018-06-03 18:42:41 +0200

    script 700 was missing two letters "fi"

--- Personal/700-apps-for-screenshots.sh
+++ Personal/700-apps-for-screenshots.sh
@@ -292,7 +292,6 @@ fi
 
 
 package="curseradio-git"
-echo "Installing $package"
 
 #----------------------------------------------------------------------------------
 
@@ -338,55 +337,8 @@ else
 
 	fi
 
+fi
 
-	package="slurm"
-	echo "Installing $package"
-
-	#----------------------------------------------------------------------------------
-
-	#checking if application is already installed or else install with aur helpers
-	if pacman -Qi $package &> /dev/null; then
-
-		echo "################################################################"
-		echo "################## "$package" is already installed"
-		echo "################################################################"
-
-	else
-
-		#checking which helper is installed
-		if pacman -Qi yaourt &> /dev/null; then
-
-			echo "Installing with yaourt"
-			yaourt -S --noconfirm $package
-
-		elif pacman -Qi pacaur &> /dev/null; then
-
-			echo "Installing with pacaur"
-			pacaur -S --noconfirm --noedit  $package
-
-		elif pacman -Qi packer &> /dev/null; then
-
-			echo "Installing with packer"
-			packer -S --noconfirm --noedit  $package
-
-		fi
-
-		# Just checking if installation was successful
-		if pacman -Qi $package &> /dev/null; then
-
-		echo "################################################################"
-		echo "#########  "$package" has been installed"
-		echo "################################################################"
-
-		else
-
-		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-		echo "!!!!!!!!!  "$package" has NOT been installed"
-		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-		fi
-
-	fi
 
 
 echo "################################################################"
