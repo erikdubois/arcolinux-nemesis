653d81643b555da471ee39c445027bdaed97f7db
Erik Dubois <erik.dubois@gmail.com>
2018-06-21 11:22:35 +0200

    plasma specific script added

---
+++ Personal/900-plasma-specific-applications-v1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website   :   https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "Installing extra icons, themes, applications not on dvd"
+
+sh ../AUR/install-numix-kde-theme-git-v*.sh
+
+echo "################################################################"
+echo "####                 EXTRA SOFTWARE INSTALLED             ######"
+echo "################################################################"
