0fd67c75edbbd0cb7aa06ad8eb786bd4241bde26
Erik Dubois <erik.dubois@gmail.com>
2018-08-29 20:07:08 +0200

    update

--- 200-software-AUR-repo-v3.sh
+++ 200-software-AUR-repo-v3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #set -e
 ##################################################################################################################
-# Author 	: Erik Dubois
-# Website : https://www.erikdubois.be
+# Author 	: 	Erik Dubois
+# Website	: 	https://www.erikdubois.be
 # Website	:	https://www.arcolinux.info
 # Website	:	https://www.arcolinux.com
 # Website	:	https://www.arcolinuxd.com
@@ -16,16 +16,11 @@
 # software from AUR (Arch User Repositories)
 # https://aur.archlinux.org/packages/
 
-#sh AUR/install-aurman-v*.sh
-#sh AUR/install-discord-v*.sh
-#sh AUR/install-dropbox-v*.sh
-#sh AUR/install-insync-v*.sh
 sh AUR/install-mpd-ncmpcpp-v*.sh
 sh AUR/install-numix-folders-git-v*.sh
 sh AUR/install-oxy-neon-v*.sh
 sh AUR/install-radiotray-v*.sh
 sh AUR/install-sardi-extra-icons-v*.sh
-#sh AUR/install-spotify-v*.sh
 sh AUR/install-virtualbox-for-linux-v*.sh
 #sh AUR/install-vivaldi-snapshot-v*.sh
 #sh AUR/install-whatsapp-web-desktop-v*.sh
