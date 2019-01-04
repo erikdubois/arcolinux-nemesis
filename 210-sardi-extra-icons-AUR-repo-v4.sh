5bb37c1f33858547dc0699b2de99b7d1d5586b28
Erik Dubois <erik.dubois@gmail.com>
2019-01-04 17:04:28 +0100

    sardi-icons-extra script

--- 210-sardi-extra-icons-AUR-repo-v4.sh
+++ 210-sardi-extra-icons-AUR-repo-v4.sh
@@ -16,18 +16,7 @@
 # software from AUR (Arch User Repositories)
 # https://aur.archlinux.org/packages/
 
-sh AUR/install-insync-v*.sh
-sh AUR/install-mpd-ncmpcpp-v*.sh
-sh AUR/install-radiotray-v*.sh
-sh AUR/install-the-platinum-searcher-bin-v*.sh
-sh AUR/install-virtualbox-for-linux-v*.sh
-sh AUR/install-vivaldi-widevine-v*.sh
-
-# these come last always
-echo "Checking if icons from applications have a hardcoded path"
-echo "and fixing them"
-echo "Wait for it ..."
-sudo hardcode-fixer
+sh AUR/install-sardi-extra-icons-v*.sh
 
 echo "################################################################"
 echo "####        Software from AUR Repository installed        ######"
