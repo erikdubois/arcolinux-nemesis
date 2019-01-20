78d448bc5e44e4a225a32f331e71d079428f3996
Erik Dubois <erik.dubois@gmail.com>
2019-01-20 17:09:40 +0100

    keyserver script update

--- Personal/keyservers/10-add-keyservers-for-key-importing-v4.sh
+++ Personal/keyservers/10-add-keyservers-for-key-importing-v4.sh
@@ -6,6 +6,8 @@ set -e
 # Website	:	https://www.arcolinux.info
 # Website	:	https://www.arcolinux.com
 # Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
 # Website	:	https://www.arcolinuxforum.com
 ##################################################################################################################
 #
@@ -14,14 +16,23 @@ set -e
 ##################################################################################################################
 [ -d $HOME"/.gnupg" ] || mkdir -p $HOME"/.gnupg"
 
-echo '
+echo "Adding keyservers to your personal .gpg for future applications"
+echo "that require keys to be imported with yay for example"
 
+echo '
 keyserver hkp://pool.sks-keyservers.net:80
 keyserver hkps://hkps.pool.sks-keyservers.net:443
 keyserver hkp://ipv4.pool.sks-keyservers.net:11371' | tee --append ~/.gnupg/gpg.conf
 
 chmod 600 ~/.gnupg/gpg.conf
 chmod 700 ~/.gnupg
+
+echo "Adding keyservers to the /etc/pacman.d/gnupg folder for the use with pacman"
+
+echo '
+keyserver hkp://pool.sks-keyservers.net:80
+keyserver hkps://hkps.pool.sks-keyservers.net:443
+keyserver hkp://ipv4.pool.sks-keyservers.net:11371' | sudo tee --append /etc/pacman.d/gnupg/gpg.conf
 
 echo "################################################################"
 echo "###                  keyservers added                       ####"
