1ba9dae446e69184f083af45f2283b965bf90d04
Erik Dubois <erik.dubois@gmail.com>
2019-02-22 12:03:19 +0100

    mint-y themes added

---
+++ 230-surfn-mint-y-icons-git-AUR-repo-v1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website	: 	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# software from AUR (Arch User Repositories)
+# https://aur.archlinux.org/packages/
+
+sh AUR/install-surfn-mint-y-icons-git-v*.sh
+
+echo "################################################################"
+echo "####        Software from AUR Repository installed        ######"
+echo "################################################################"
---
+++ AUR/uninstall-all-icons-sardi-mint-y-icons-v1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sudo pacman -R sardi-ghost-flexible-mint-y-icons-git --noconfirm
+sudo pacman -R sardi-flexible-mint-y-icons-git --noconfirm
+sudo pacman -R sardi-orb-colora-mint-y-icons-git --noconfirm
+sudo pacman -R sardi-flat-mint-y-icons-git --noconfirm
+sudo pacman -R sardi-mono-mint-y-icons-git --noconfirm
+sudo pacman -R sardi-mint-y-icons-git --noconfirm
