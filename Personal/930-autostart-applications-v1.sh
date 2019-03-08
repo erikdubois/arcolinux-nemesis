651b178203b13c74c67d429acd6642d0ff76898a
Erik Dubois <erik.dubois@gmail.com>
2019-03-08 11:13:47 +0100

    autostart

---
+++ Personal/930-autostart-applications-v1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+[ -d $HOME"/.config/autostart" ] || mkdir -p $HOME"/.config/autostart"
+
+sleep 1
+cp -f settings/autostart/* $HOME"/.config/autostart"
+
+echo "################################################################"
+echo "####                      installed                       ######"
+echo "################################################################"
