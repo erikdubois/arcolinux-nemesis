3ad82685aecd4f8dcbba1db036cc9a226ef1b575
Erik Dubois <erik.dubois@gmail.com>
2019-04-09 08:58:55 +0200

    666 delete applications update

--- Personal/666-uninstall-applications.sh
+++ Personal/666-uninstall-applications.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#set -e
 ##################################################################################################################
 # Author	:	Erik Dubois
 # Website	:	https://www.erikdubois.be
@@ -23,14 +23,47 @@ set -e
 #ArcoLinuxD = you run the scripts you want (including or excluding software)
 #ArcoLinuxB = you add or delete the packages you want on the iso
 
-sudo pacman -R --noconfirm evolution firefox galculator geany gimp guvcview
-sudo pacman -R --noconfirm inkscape meld pragha qbittorrent simplescreenrecorder
-sudo pacman -R --noconfirm variety vlc
-sudo pacman -R --noconfirm baobab gparted gpick plank
-sudo pacman -R --noconfirm timeshift powertop nomacs youtube-dl
+sudo pacman -R --noconfirm evolution
+sudo pacman -R --noconfirm firefox
+sudo pacman -R --noconfirm galculator
+sudo pacman -R --noconfirm geany 
+sudo pacman -R --noconfirm gimp
+sudo pacman -R --noconfirm guvcview
+sudo pacman -R --noconfirm inkscape
+sudo pacman -R --noconfirm meld
+sudo pacman -R --noconfirm pragha
+sudo pacman -R --noconfirm qbittorrent
+sudo pacman -R --noconfirm simplescreenrecorder
+sudo pacman -R --noconfirm variety
+sudo pacman -R --noconfirm vlc
+sudo pacman -R --noconfirm atom
+sudo pacman -R --noconfirm baobab
+sudo pacman -R --noconfirm gparted
+sudo pacman -R --noconfirm gpick
+sudo pacman -R --noconfirm plank
+sudo pacman -R --noconfirm timeshift
+sudo pacman -R --noconfirm powertop
+sudo pacman -R --noconfirm nomacs
+sudo pacman -R --noconfirm youtube-dl
 sudo pacman -R --noconfirm chromium
-sudo pacman -R --noconfirm halo-icons-git moka-icon-theme numix-circle-arc-icons-git numix-icon-theme-git numix-circle-icon-theme-git zafiro-icon-theme
-sudo pacman -R --noconfirm font-manager grub-customizer peek screenkey temps wttr
+sudo pacman -R --noconfirm redshift-gtk
+sudo pacman -R --noconfirm redshift
+sudo pacman -R --noconfirm halo-icons-git
+sudo pacman -R --noconfirm moka-icon-theme
+sudo pacman -R --noconfirm numix-circle-arc-icons-git
+sudo pacman -R --noconfirm numix-icon-theme-git
+sudo pacman -R --noconfirm numix-circle-icon-theme-git
+sudo pacman -R --noconfirm zafiro-icon-theme
+sudo pacman -R --noconfirm font-manager
+sudo pacman -R --noconfirm grub-customizer
+sudo pacman -R --noconfirm peek
+sudo pacman -R --noconfirm screenkey
+sudo pacman -R --noconfirm temps
+sudo pacman -R --noconfirm wttr
+sudo pacman -R --noconfirm xfburn
+sudo pacman -R --noconfirm grsync
+sudo pacman -R --noconfirm gnome-software gnome-software-packagekit-plugin
+sudo pacman -R --noconfirm pulseaudio-equalizer-ladspa
 #sudo pacman -R --noconfirm
 
 echo "################################################################"
