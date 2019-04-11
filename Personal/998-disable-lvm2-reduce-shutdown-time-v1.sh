a80c101c2fbad323da4c6fec63645a92a7f11c77
Erik Dubois <erik.dubois@gmail.com>
2019-04-11 08:11:57 +0200

    option 4 in 90 sec to 0

--- Personal/998-disable-lvm2-reduce-shutdown-time-v1.sh
+++ Personal/998-disable-lvm2-reduce-shutdown-time-v1.sh
@@ -18,13 +18,25 @@ set -e
 FIND="use_lvmetad = 1"
 REPLACE="use_lvmetad = 0"
 
+echo "#############"
+echo "Option 1"
+echo "#############"
 find /etc/lvm/lvm.conf -type f -exec sudo sed -i "s/$FIND/$REPLACE/g" {} \;
-
+echo "#############"
+echo "Option 2"
+echo "#############"
 sudo systemctl stop lvm2-lvmetad.socket lvm2-lvmetad.service
 sudo systemctl disable lvm2-lvmetad.socket lvm2-lvmetad.service
-
+echo "#############"
+echo "Option 3"
+echo "#############"
 sudo systemctl mask lvm2-monitor
-
+echo "#############"
+echo "Option 4"
+echo "#############"
+FIND="#DefaultTimeoutStopSec=90s"
+REPLACE="DefaultTimeoutStopSec=30s"
+find /etc/systemd/system.conf -type f -exec sudo sed -i "s/$FIND/$REPLACE/g" {} \;
 echo "################################################################"
 echo "####                      reboot to 0 seconds             ######"
 echo "################################################################"
