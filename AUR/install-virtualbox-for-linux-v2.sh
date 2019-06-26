7ee107dbd2183522727a11dea3fca05dadd41f81
Erik Dubois <erik.dubois@gmail.com>
2019-06-26 15:49:02 +0200

    update

--- AUR/install-virtualbox-for-linux-v2.sh
+++ AUR/install-virtualbox-for-linux-v2.sh
@@ -21,7 +21,7 @@ echo "################################################################"
 
 sudo pacman -S --needed --noconfirm virtualbox-host-modules-arch
 sudo pacman -S --noconfirm --needed virtualbox
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+#sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "################################################################"
 echo "#########           You got to reboot.                 #########"
