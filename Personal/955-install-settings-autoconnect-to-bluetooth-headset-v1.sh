26d60f0f32db055405605d590e537aa45c388e95
Erik Dubois <erik.dubois@gmail.com>
2019-07-17 18:12:05 +0200

    bluetooth conf

--- Personal/955-install-settings-autoconnect-to-bluetooth-headset-v1.sh
+++ Personal/955-install-settings-autoconnect-to-bluetooth-headset-v1.sh
@@ -16,14 +16,29 @@
 #settings for a bluetooth headset
 #after reboot switch off and on your device to get connected automatically and to the right channel
 
+echo "Fix 1"
+echo "#################"
 FIND="#AutoEnable=false"
 REPLACE="AutoEnable=true"
 sudo sed -i "s/$FIND/$REPLACE/g" /etc/bluetooth/main.conf
 
+if grep --quiet AutoEnable=true /etc/bluetooth/main.conf; then
+  echo "Autoenable=true is already added"
+fi
+
+echo "Fix 2"
+echo "#################"
+if grep --quiet module-switch-on-connect /etc/pulse/default.pa; then
+  echo "module-switch-on-connect is already added"
+else
 echo '
 load-module module-switch-on-connect' | sudo tee --append /etc/pulse/default.pa
+fi
 
-echo 'options ath9k btcoex_enable = 1' | sudo tee /etc/modprobe.d/bluetooth-clear.conf
+echo "Fix 3"
+echo "#################"
+[ -f /etc/modprobe.d/bluetooth-clear.conf ] && echo "Bluetooth-clear already created" || echo 'options ath9k btcoex_enable = 1' | sudo tee /etc/modprobe.d/bluetooth-clear.conf
+
 
 echo "################################################################"
 echo "#########   reboot to let the settings kick in  ################"
