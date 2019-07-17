5b926bbc53251a9503e45f2d43255b9f85f02eff
Erik Dubois <erik.dubois@gmail.com>
2019-07-17 16:39:21 +0200

    bluetooth headset

---
+++ Personal/955-install-settings-autoconnect-to-bluetooth-headset-v1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#settings for a bluetooth headset
+#after reboot switch off and on your device to get connected automatically and to the right channel
+
+FIND="#AutoEnable=false"
+REPLACE="AutoEnable=true"
+sudo sed -i "s/$FIND/$REPLACE/g" /etc/bluetooth/main.conf
+
+echo '
+load-module module-switch-on-connect' | sudo tee --append /etc/pulse/default.pa
+
+echo "################################################################"
+echo "#########   reboot to let the settings kick in  ################"
+echo "################################################################"
