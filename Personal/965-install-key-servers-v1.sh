b2c847e8d69da6eb1dcadb7cf4ad1de59f223ff4
Erik Dubois <erik.dubois@gmail.com>
2019-07-27 09:52:27 +0200

    keyservers and ckb-next

---
+++ Personal/965-install-key-servers-v1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+cd keyservers
+sh 10-add-keyservers-for-key-importing-v*
+
+echo "################################################################"
+echo "#########             keyservers added          ################"
+echo "################################################################"
