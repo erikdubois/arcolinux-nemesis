1a122423855cbc13989464266b121865fe4c3c05
Erik Dubois <erik.dubois@gmail.com>
2019-10-19 09:20:50 +0200

    layout

--- AUR/install-steam-intel-gpu-v1.sh
+++ AUR/install-steam-intel-gpu-v1.sh
@@ -15,33 +15,27 @@ set -e
 #
 ##################################################################################################################
 
-sudo pacman -S  lib32-libvdpau \
- 								lib32-libva \
-								lib32-nvidia-utils \
-								lib32-libxtst \
-								lib32-libxrandr \
-								lib32-libpulse \
-								lib32-gdk-pixbuf2 \
-								lib32-gtk2 \
-								lib32-openal \
-								lib32-mesa \
-								lib32-gcc-libs \
-								lib32-libx11 \
-								lib32-libxss \
-								lib32-alsa-plugins \
-								lsof \
-								lib32-libgpg-error \
-								lib32-libindicator-gtk2 \
-								lib32-libdbusmenu-glib \
-								lib32-libdbusmenu-gtk2 \
-								lib32-nss
-
-
-
-
-
-
-
+sudo pacman -S  \
+lib32-libvdpau \
+lib32-libva \
+lib32-nvidia-utils \
+lib32-libxtst \
+lib32-libxrandr \
+lib32-libpulse \
+lib32-gdk-pixbuf2 \
+lib32-gtk2 \
+lib32-openal \
+lib32-mesa \
+lib32-gcc-libs \
+lib32-libx11 \
+lib32-libxss \
+lib32-alsa-plugins \
+lsof \
+lib32-libgpg-error \
+lib32-libindicator-gtk2 \
+lib32-libdbusmenu-glib \
+lib32-libdbusmenu-gtk2 \
+lib32-nss
 
 package="steam"
 
