091a7db9f8528229b5bd3917b0570ccea0d752cc
Erik Dubois <erik.dubois@gmail.com>
2019-10-29 21:12:31 +0100

    qt environment

---
+++ AUR/install-qt-development-environment-v1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sudo pacman -S qt5-base qt5-doc qt5-tools qtcreator --noconfirm --needed
