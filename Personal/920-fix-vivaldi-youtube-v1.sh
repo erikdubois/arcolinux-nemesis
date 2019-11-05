d53f9434f7a5b1727c9ef841429036b0a4a85646
Erik Dubois <erik.dubois@gmail.com>
2019-11-05 07:22:18 +0100

    vivaldi fix

---
+++ Personal/920-fix-vivaldi-youtube-v1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "fixing vivaldi not playing youtube video's"
+echo "following packages are installed vivaldi-widevine and vivaldi-codecs-ffmpeg-extra-bin"
+
+sudo /opt/vivaldi/update-ffmpeg
