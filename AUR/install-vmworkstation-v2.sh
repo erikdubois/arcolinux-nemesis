40a3acab6a7700c908beff6a7caea343e821e058
Erik Dubois <erik.dubois@gmail.com>
2019-12-05 08:13:20 +0100

    vmware update

--- AUR/install-vmworkstation-v2.sh
+++ AUR/install-vmworkstation-v2.sh
@@ -17,64 +17,64 @@ set -e
 
 #gpg --keyserver hkps://pgp.mit.edu:443 --recv-keys C52048C0C0748FEE227D47A2702353E0F7E48EDB
 
-package="ncurses5-compat-libs"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-		tput setaf 2
-		echo "################################################################"
-		echo "################## "$package" is already installed"
-		echo "################################################################"
-		tput sgr0
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi yay &> /dev/null; then
-
-		tput setaf 3
-		echo "################################################################"
-		echo "######### Installing with yay"
-		echo "################################################################"
-		tput sgr0
-
-		yay -S --noconfirm $package
-
-	elif pacman -Qi trizen &> /dev/null; then
-
-		tput setaf 3
-		echo "################################################################"
-		echo "######### Installing with trizen"
-		echo "################################################################"
-		tput sgr0
-		trizen -S --noconfirm --needed --noedit $package
-
-	fi
-
-fi
-
-
-# Just checking if installation was successful
-if pacman -Qi $package &> /dev/null; then
-
-	tput setaf 2
-	echo "################################################################"
-	echo "#########  Checking ..."$package" has been installed"
-	echo "################################################################"
-	tput sgr0
-
-else
-
-	tput setaf 1
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	echo "!!!!!!!!!  "$package" has NOT been installed"
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	tput sgr0
-
-fi
+# package="ncurses5-compat-libs"
+#
+# #----------------------------------------------------------------------------------
+#
+# #checking if application is already installed or else install with aur helpers
+# if pacman -Qi $package &> /dev/null; then
+#
+# 		tput setaf 2
+# 		echo "################################################################"
+# 		echo "################## "$package" is already installed"
+# 		echo "################################################################"
+# 		tput sgr0
+#
+# else
+#
+# 	#checking which helper is installed
+# 	if pacman -Qi yay &> /dev/null; then
+#
+# 		tput setaf 3
+# 		echo "################################################################"
+# 		echo "######### Installing with yay"
+# 		echo "################################################################"
+# 		tput sgr0
+#
+# 		yay -S --noconfirm $package
+#
+# 	elif pacman -Qi trizen &> /dev/null; then
+#
+# 		tput setaf 3
+# 		echo "################################################################"
+# 		echo "######### Installing with trizen"
+# 		echo "################################################################"
+# 		tput sgr0
+# 		trizen -S --noconfirm --needed --noedit $package
+#
+# 	fi
+#
+# fi
+#
+#
+# # Just checking if installation was successful
+# if pacman -Qi $package &> /dev/null; then
+#
+# 	tput setaf 2
+# 	echo "################################################################"
+# 	echo "#########  Checking ..."$package" has been installed"
+# 	echo "################################################################"
+# 	tput sgr0
+#
+# else
+#
+# 	tput setaf 1
+# 	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+# 	echo "!!!!!!!!!  "$package" has NOT been installed"
+# 	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+# 	tput sgr0
+#
+# fi
 
 #----------------------------------------------------------------------------------
 
