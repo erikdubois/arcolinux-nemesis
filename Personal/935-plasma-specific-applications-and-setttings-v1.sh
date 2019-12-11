f760649a36ad4de2ca6a7634d8fbb4de09150dae
Erik Dubois <erik.dubois@gmail.com>
2019-12-11 22:11:44 +0100

    plasma qt5

--- Personal/935-plasma-specific-applications-and-setttings-v1.sh
+++ Personal/935-plasma-specific-applications-and-setttings-v1.sh
@@ -19,14 +19,17 @@ installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 cp $installed_dir/settings/plasma/bookmarks/user-places.xbel ~/.local/share/user-places.xbel
 
 sudo pacman -R xcursor-breeze --noconfirm
-sudo pacman -R kvantum-qt5 kvantum-theme-arc arcolinux-kvantum-git arcolinux-config-git --noconfirm
+sudo pacman -R kvantum-qt5 kvantum-theme-arc arcolinux-kvantum-git arcolinux-config-git arcolinux-qt5-git --noconfirm
+sudo pacman -R arcolinux-qt5-git --noconfirm
 sudo pacman -S plasma-meta kde-applications-meta --noconfirm --needed
 sudo pacman -S arcolinux-plasma-nemesis-git --noconfirm --needed
 sudo pacman -S surfn-arc-breeze-icons-git --noconfirm --needed
 sudo pacman -S arcolinux-arc-kde --noconfirm --needed
 sudo pacman -S arcolinux-config-plasma-nemesis-git --noconfirm --needed
+sudo pacman -S arcolinux-qt5-plasma-git --noconfirm --needed
 
-echo "do a skel"
+echo "copy /etc/skel to home"
+cp -rT /etc/skel ~
 
 echo "################################################################"
 echo "####                 PLASMA  INSTALLED                    ######"
