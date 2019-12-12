6849ad5427f768542159eabf4ffc5dcbac43d14b
Erik Dubois <erik.dubois@gmail.com>
2019-12-12 08:06:05 +0100

    improving plasma script - getting nemesis packages

--- Personal/935-plasma-specific-applications-and-setttings-v1.sh
+++ Personal/935-plasma-specific-applications-and-setttings-v1.sh
@@ -19,14 +19,26 @@ installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 cp $installed_dir/settings/plasma/bookmarks/user-places.xbel ~/.local/share/user-places.xbel
 
 sudo pacman -R xcursor-breeze qt5ct --noconfirm
-sudo pacman -R kvantum-qt5 kvantum-theme-arc arcolinux-kvantum-git arcolinux-config-git arcolinux-qt5-git --noconfirm
-sudo pacman -R arcolinux-qt5-git --noconfirm
-sudo pacman -S plasma-meta kde-applications-meta --noconfirm --needed
+
+sudo pacman -R kvantum-theme-arc --noconfirm
+sudo pacman -R kvantum-qt5 --noconfirm
+sudo pacman -R kvantum-theme-arc --noconfirm
+sudo pacman -R arcolinux-kvantum-git --noconfirm
+
+sudo pacman -R arcolinux-plasma-git --noconfirm
 sudo pacman -S arcolinux-plasma-nemesis-git --noconfirm --needed
+
+sudo pacman -R arcolinux-config-git --noconfirm
+sudo pacman -R arcolinux-config-plasma-git --noconfirm
+sudo pacman -S arcolinux-config-plasma-nemesis-git --noconfirm --needed
+
+
+sudo pacman -R arcolinux-qt5-git --noconfirm
+sudo pacman -S arcolinux-qt5-plasma-git --noconfirm --needed
+
+sudo pacman -S plasma-meta kde-applications-meta --noconfirm --needed
 sudo pacman -S surfn-arc-breeze-icons-git --noconfirm --needed
 sudo pacman -S arcolinux-arc-kde --noconfirm --needed
-sudo pacman -S arcolinux-config-plasma-nemesis-git --noconfirm --needed
-sudo pacman -S arcolinux-qt5-plasma-git --noconfirm --needed
 
 echo "copy /etc/skel to home"
 cp -rT /etc/skel ~
