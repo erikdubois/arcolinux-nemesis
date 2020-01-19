eceba03be6d2fe4ba2d39f77f554714e5003bcd4
Erik Dubois <erik.dubois@gmail.com>
2020-01-19 07:23:37 +0100

    s gone in remove

--- Personal/667-uninstall-arcolinux-meta-steam.sh
+++ Personal/667-uninstall-arcolinux-meta-steam.sh
@@ -15,7 +15,7 @@
 #
 ##################################################################################################################
 
-sudo pacman -Rs arcolinux-meta-steam steam lib32-libvdpau lib32-libva lib32-nvidia-utils lib32-libxtst lib32-libxrandr lib32-libpulse lib32-gdk-pixbuf2 \
+sudo pacman -R arcolinux-meta-steam steam lib32-libvdpau lib32-libva lib32-nvidia-utils lib32-libxtst lib32-libxrandr lib32-libpulse lib32-gdk-pixbuf2 \
 lib32-gtk2 lib32-openal lib32-mesa lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins lsof lib32-libgpg-error \
 lib32-libindicator-gtk2 lib32-libdbusmenu-glib lib32-libdbusmenu-gtk2 lib32-nss
 echo "################################################################"
