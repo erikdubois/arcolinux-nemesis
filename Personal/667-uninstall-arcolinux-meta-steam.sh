431694f49c83ad9f0a242c175201c8f0171f2cce
Erik Dubois <erik.dubois@gmail.com>
2020-01-19 07:42:24 +0100

    lsof and ttf ms fonts in steam script

--- Personal/667-uninstall-arcolinux-meta-steam.sh
+++ Personal/667-uninstall-arcolinux-meta-steam.sh
@@ -22,6 +22,9 @@ echo "  BUT IT IS THE ONLY WAY TO GET RID OF ALL THESE PACKAGES";tput sgr0
 sudo pacman -Rs arcolinux-meta-steam steam lib32-libvdpau lib32-libva lib32-nvidia-utils lib32-libxtst lib32-libxrandr lib32-libpulse lib32-gdk-pixbuf2 \
 lib32-gtk2 lib32-openal lib32-mesa lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins lsof lib32-libgpg-error \
 lib32-libindicator-gtk2 lib32-libdbusmenu-glib lib32-libdbusmenu-gtk2 lib32-nss
+
+# fixing what was broken after the uninstall
+sudo pacman -S ttf-ms-fonts lsof --noconfirm
 echo "################################################################"
 echo "####                      packages uninstalled            ######"
 echo "################################################################"
