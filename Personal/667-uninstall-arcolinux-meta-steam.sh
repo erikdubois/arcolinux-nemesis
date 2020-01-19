7e2700175506ad44ffe098d0e37e254dfa04a652
Erik Dubois <erik.dubois@gmail.com>
2020-01-19 07:29:38 +0100

    typo

--- Personal/667-uninstall-arcolinux-meta-steam.sh
+++ Personal/667-uninstall-arcolinux-meta-steam.sh
@@ -17,7 +17,7 @@
 
 tput setaf 3;echo "  DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK."
 echo "  THIS MAY BRICK YOUR SYSTEM"
-echo "  BUT IT IS THE ONLY WAY TO GET RID OF ALL OF THESE PACKAGES";tput sgr0
+echo "  BUT IT IS THE ONLY WAY TO GET RID OF ALL THESE PACKAGES";tput sgr0
 
 sudo pacman -Rs arcolinux-meta-steam steam lib32-libvdpau lib32-libva lib32-nvidia-utils lib32-libxtst lib32-libxrandr lib32-libpulse lib32-gdk-pixbuf2 \
 lib32-gtk2 lib32-openal lib32-mesa lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins lsof lib32-libgpg-error \
