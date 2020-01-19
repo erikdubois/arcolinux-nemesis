37e4ac771417a68f5a36d05d9b4d7310cb150cad
Erik Dubois <erik.dubois@gmail.com>
2020-01-19 07:27:38 +0100

    some color in steam script

--- Personal/667-uninstall-arcolinux-meta-steam.sh
+++ Personal/667-uninstall-arcolinux-meta-steam.sh
@@ -15,7 +15,11 @@
 #
 ##################################################################################################################
 
-sudo pacman -R arcolinux-meta-steam steam lib32-libvdpau lib32-libva lib32-nvidia-utils lib32-libxtst lib32-libxrandr lib32-libpulse lib32-gdk-pixbuf2 \
+tput setaf 3;echo "  DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK."
+echo "  THIS MAY BRICK YOUR SYSTEM"
+echo "  BUT IT IS THE ONLY WAY TO GET RID OF ALL OF THESE PACKAGES";tput sgr0
+
+sudo pacman -Rs arcolinux-meta-steam steam lib32-libvdpau lib32-libva lib32-nvidia-utils lib32-libxtst lib32-libxrandr lib32-libpulse lib32-gdk-pixbuf2 \
 lib32-gtk2 lib32-openal lib32-mesa lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins lsof lib32-libgpg-error \
 lib32-libindicator-gtk2 lib32-libdbusmenu-glib lib32-libdbusmenu-gtk2 lib32-nss
 echo "################################################################"
