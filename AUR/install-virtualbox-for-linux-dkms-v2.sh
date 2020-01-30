2dbc0ec28aa4c92ae8b4fe6aeba969f3cc872e00
Erik Dubois <erik.dubois@gmail.com>
2020-01-30 19:38:58 +0100

    linux-headers added

--- AUR/install-virtualbox-for-linux-dkms-v2.sh
+++ AUR/install-virtualbox-for-linux-dkms-v2.sh
@@ -19,6 +19,7 @@ echo "##  This script assumes you have the linux kernel running     ##"
 echo "################################################################"
 
 sudo pacman -S --noconfirm --needed virtualbox
+sudo pacman -S --noconfirm --needed linux-headers
 sudo pacman -S --needed virtualbox-host-dkms
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
