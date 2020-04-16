7680c37f2b40883a17f4adbed5647c3fd806a7ba
Erik Dubois <erik.dubois@gmail.com>
2020-04-16 08:45:04 +0200

    999 for jwm

--- Personal/999-install-settings-for-X11-intel-ONLY-FOR-SSD-NOT-VIRTUAL-BOX.sh
+++ Personal/999-install-settings-for-X11-intel-ONLY-FOR-SSD-NOT-VIRTUAL-BOX.sh
@@ -34,16 +34,19 @@ echo "3.  Bspwm"
 echo "4.  Budgie"
 echo "5.  Cinnamon"
 echo "6.  Deepin"
-echo "7.  Gnome"
-echo "8.  Herbstlufwm"
-echo "9.  i3"
-echo "10. LXQt"
-echo "11. Mate"
-echo "12. Openbox"
-echo "13. Plasma"
-echo "14. Qtile"
-echo "15. Xfce"
-echo "16. Xmonad"
+echo "7.  Dwm"
+echo "8.  Gnome"
+echo "9.  Herbstlufwm"
+echo "10. i3"
+echo "11. jwm"
+echo "12. LXQt"
+echo "13. Mate"
+echo "14. Openbox"
+echo "15. Plasma"
+echo "16. Qtile"
+echo "17. Spectrwm"
+echo "18. Xfce"
+echo "19. Xmonad"
 echo "Type the number..."
 
 read CHOICE
@@ -83,9 +86,6 @@ case $CHOICE in
     6 )
       sudo cp $WDP/$SETTING2 $DESTINATION1
       ;;
-    7 )
-      sudo cp $WDP/$SETTING0 $DESTINATION1
-      ;;
     8 )
       sudo cp $WDP/$SETTING0 $DESTINATION1
       ;;
@@ -93,27 +93,36 @@ case $CHOICE in
       sudo cp $WDP/$SETTING0 $DESTINATION1
       ;;
     10 )
-      #sudo cp $WDP/$SETTING0 $DESTINATION1
+      sudo cp $WDP/$SETTING0 $DESTINATION1
       ;;
     11 )
       sudo cp $WDP/$SETTING0 $DESTINATION1
       ;;
     12 )
-      sudo cp $WDP/$SETTING0 $DESTINATION1
+      #sudo cp $WDP/$SETTING0 $DESTINATION1
       ;;
     13 )
+      sudo cp $WDP/$SETTING0 $DESTINATION1
+      ;;
+    14 )
+      sudo cp $WDP/$SETTING0 $DESTINATION1
+      ;;
+    15 )
       #if [ -f "~/.config/kwinrc" ] ;
       #then cp ~/.config/kwinrc ~/.config/kwinrc-backup
       #fi
       #cp $WDP/settings/kwinrc/kwinrc ~/.config/kwinrc
       ;;
-    14 )
+    16 )
       sudo cp $WDP/$SETTING1 $DESTINATION1
       ;;
-    15 )
+    17 )
+      sudo cp $WDP/$SETTING1 $DESTINATION1
+      ;;
+    18 )
       sudo cp $WDP/$SETTING0 $DESTINATION1
       ;;
-    16 )
+    19 )
       sudo cp $WDP/$SETTING2 $DESTINATION1
       ;;
     * )
