48ec8f596420003b07fd8c6ddad9ee5907e222a6
Erik Dubois <erik.dubois@gmail.com>
2021-01-06 19:57:17 +0100

    one space

--- Personal/settings/nvidia/nvidia-msi-gt70-gtx-670m.sh
+++ Personal/settings/nvidia/nvidia-msi-gt70-gtx-670m.sh
@@ -17,7 +17,7 @@
 
 tput setaf 3;echo "  DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK."
 echo "  THIS MAY BRICK YOUR SYSTEM"
-echo " REUSE THE SCRIPT TO MAKE YOUR OWN SCRIPT"
+echo "  REUSE THE SCRIPT TO MAKE YOUR OWN SCRIPT"
 tput sgr0
 
 sudo pacman -S --noconfirm --needed linux-lts 
