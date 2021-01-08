0060bee0d3f817dc4ee608466ace9c8d746b94d7
Erik Dubois <erik.dubois@gmail.com>
2021-01-08 21:27:31 +0100

    extra nvidia script

---
+++ Personal/settings/nvidia/nvidia-hybrid-390-akoya-gt-620m.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+tput setaf 3;echo "  DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK."
+echo "  THIS MAY BRICK YOUR SYSTEM"
+echo "  REUSE THE SCRIPT TO MAKE YOUR OWN SCRIPT"
+tput sgr0
+
+sudo pacman -S --noconfirm --needed linux-lts 
+sudo pacman -S --noconfirm --needed linux-lts-headers
+sudo pacman -S --noconfirm --needed optimus-manager-qt
+yay -S nvidia-390xx-dkms
+yay -S nvidia-390xx-settings
+yay -S nvidia-390xx-utils
+echo "################################################################"
+echo "####                      packages installed              ######"
+echo "################################################################"
