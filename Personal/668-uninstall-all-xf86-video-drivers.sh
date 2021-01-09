d8673e9d8c9d517d42a08b4d3cf2c619fdbd30c2
Erik Dubois <erik.dubois@gmail.com>
2021-01-09 13:55:58 +0100

    uninstall xf86 script

---
+++ Personal/668-uninstall-all-xf86-video-drivers.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+tput setaf 3;echo "  DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK."
+echo "  THIS MAY BRICK YOUR SYSTEM";tput sgr0
+
+sudo pacman -R --noconfirm xf86-video-amdgpu
+sudo pacman -R --noconfirm xf86-video-ati
+sudo pacman -R --noconfirm xf86-video-fbdev
+sudo pacman -R --noconfirm xf86-video-intel
+sudo pacman -R --noconfirm xf86-video-nouveau
+sudo pacman -R --noconfirm xf86-video-openchrome
+sudo pacman -R --noconfirm xf86-video-vesa
+sudo pacman -R --noconfirm xf86-video-vmware
+
+echo "################################################################"
+echo "####                      packages uninstalled            ######"
+echo "################################################################"
