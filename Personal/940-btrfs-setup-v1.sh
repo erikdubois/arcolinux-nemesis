4d81dea508f46a3906a779708c63903a47191b2f
Erik Dubois <erik.dubois@gmail.com>
2021-02-27 10:37:25 +0100

    check for btrfs

--- Personal/940-btrfs-setup-v1.sh
+++ Personal/940-btrfs-setup-v1.sh
@@ -13,11 +13,16 @@
 #
 ##################################################################################################################
 
-sudo pacman -S --needed --noconfirm timeshift
-sudo pacman -S --needed --noconfirm grub-btrfs
-sudo pacman -S --needed --noconfirm timeshift-autosnap
-
-sudo systemctl enable grub-btrfs.path
+if 	lsblk -f | grep btrfs > /dev/null 2>&1 ; then
+	echo "You are using BTRFS. Installing the software ..."
+	sudo pacman -S --needed --noconfirm timeshift
+	sudo pacman -S --needed --noconfirm grub-btrfs
+	sudo pacman -S --needed --noconfirm timeshift-autosnap
+	sudo systemctl enable grub-btrfs.path
+else
+	echo "Your harddisk/ssd/nvme is not formatted as BTRFS."
+	echo "Packages will not be installed"
+fi
 
 echo "################################################################"
 echo "#########   Packages installed - Reboot now     ################"
