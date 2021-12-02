ea2889cd09159c7953f7a23d2dd2e226e88d2b56
Erik Dubois <erik.dubois@gmail.com>
2021-12-02 08:39:57 +0100

    update

--- setup-git-v5.sh
+++ setup-git-v5.sh
@@ -24,15 +24,21 @@
 # git push --set-upstream origin master
 # git reset --hard orgin/master
 
-
+project=$(basename `pwd`)
+echo "-----------------------------------------------------------------------------"
+echo "this is project https://github.com/erikdubois/"$project
+echo "-----------------------------------------------------------------------------"
 git config --global pull.rebase false
 git config --global user.name "Erik Dubois"
 git config --global user.email "erik.dubois@gmail.com"
 sudo git config --system core.editor nano
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=32000'
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=32000'
 git config --global push.default simple
 
+git remote set-url origin git@github:erikdubois/$project
+
+echo "Everything set"
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
