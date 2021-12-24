6837722c48b9f3b8cb1ac6e364acd0e9a29e9dbd
arcolinuxz <arcolinuxinfo@gmail.com>
2021-12-24 09:55:17 +0100

    btrfs path scripts

--- Personal/940-btrfs-setup-v1.sh
+++ Personal/940-btrfs-setup-v1.sh
@@ -21,14 +21,6 @@ if 	lsblk -f | grep btrfs > /dev/null 2>&1 ; then
 	sudo pacman -S --needed --noconfirm grub-btrfs
 	sudo pacman -S --needed --noconfirm timeshift-autosnap
 	sudo systemctl enable grub-btrfs.path
-
-	#sudo sed -i "s/PathModified=\/.snaphots/PathModified=\/run\/timeshift\/backup\/timeshift-btrfs\/snapshots/g" /etc/systemd/system/grub-btrfs.path
-	sudo sed -i 's/Requires=\\x2esnapshots.mount/Requires=run-timeshift-backup.mount/g' /usr/lib/systemd/system/grub-btrfs.path
-	sudo sed -i 's/After=\\x2esnapshots.mount/After=run-timeshift-backup.mount/g' /usr/lib/systemd/system/grub-btrfs.path
-	sudo sed -i 's/BindsTo=\\x2esnapshots.mount/BindsTo=run-timeshift-backup.mount/g' /usr/lib/systemd/system/grub-btrfs.path
-	sudo sed -i "s/PathModified=\/.snapshots/PathModified=\/run\/timeshift\/backup\/timeshift-btrfs\/snapshots/g" /usr/lib/systemd/system/grub-btrfs.path
-	sudo sed -i "s/WantedBy=[\]x2esnapshots.mount/WantedBy=run-timeshift-backup.mount/g" /usr/lib/systemd/system/grub-btrfs.path
-	
 else
 	echo "Your harddisk/ssd/nvme is not formatted as BTRFS."
 	echo "Packages will not be installed"
