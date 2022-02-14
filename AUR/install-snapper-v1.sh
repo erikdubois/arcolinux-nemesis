caf0d635ed6216638b7b459ede0daa7fc386ad33
arcolinuxz <arcolinuxinfo@gmail.com>
2022-02-14 12:18:34 +0100

    canva config

--- AUR/install-snapper-v1.sh
+++ AUR/install-snapper-v1.sh
@@ -137,7 +137,7 @@ fi
 
 #----------------------------------------------------------------------------------
 
-package="snap-pac-grub"
+package="snapper-support"
 
 #----------------------------------------------------------------------------------
 
@@ -198,8 +198,68 @@ fi
 
 #----------------------------------------------------------------------------------
 
+package="snapper-support"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+		tput setaf 2
+		echo "################################################################"
+		echo "################## "$package" is already installed"
+		echo "################################################################"
+		tput sgr0
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi yay &> /dev/null; then
+
+		tput setaf 3
+		echo "################################################################"
+		echo "######### Installing with yay"
+		echo "################################################################"
+		tput sgr0
+
+		yay -S --noconfirm $package
+
+	elif pacman -Qi trizen &> /dev/null; then
+
+		tput setaf 3
+		echo "################################################################"
+		echo "######### Installing with trizen"
+		echo "################################################################"
+		tput sgr0
+		trizen -S --noconfirm --needed --noedit $package
+
+	fi
+
+fi
+
+
+# Just checking if installation was successful
+if pacman -Qi $package &> /dev/null; then
+
+	tput setaf 2
+	echo "################################################################"
+	echo "#########  Checking ..."$package" has been installed"
+	echo "################################################################"
+	tput sgr0
+
+else
+
+	tput setaf 1
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	tput sgr0
+
+fi
+
+
 sudo snapper -c root create-config /
 
 echo "first manual snapshot"
 
-snapper -c root create --description initial snapshot
+snapper -c root create --description "initial snapshot"
