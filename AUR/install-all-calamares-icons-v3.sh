7ac5020fdcce187f712df6eddc6d8fce2dd46dd5
arcolinuxz <arcolinuxinfo@gmail.com>
2022-02-22 08:39:22 +0100

    arcolinux icons script

---
+++ AUR/install-all-calamares-icons-v3.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#set -e
+###############################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+###############################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+###############################################################################
+
+
+###############################################################################
+#
+#   DECLARATION OF FUNCTIONS
+#
+###############################################################################
+
+
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+###############################################################################
+echo "Installation of the core software"
+###############################################################################
+
+list=(
+arc-icon-theme
+arcolinux-candy-beauty-git
+breeze
+faba-icon-theme-git
+faba-mono-icons-git
+flat-remix-git
+halo-icons-git
+la-capitaine-icon-theme-git
+moka-icon-theme-git
+numix-circle-arc-icons-git
+numix-circle-icon-theme-git
+obsidian-icon-theme
+oranchelo-icon-theme-git
+paper-icon-theme
+papirus-icon-theme
+qogir-icon-theme
+surfn-mint-y-icons-git
+tela-circle-icon-theme-git
+vimix-icon-theme-git
+we10x-icon-theme-git
+whitesur-icon-theme-git
+zafiro-icon-theme
+)
+
+count=0
+
+for name in "${list[@]}" ; do
+	count=$[count+1]
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	func_install $name
+done
+
+# onliner
+# sudo pacman -S arc-icon-theme arcolinux-candy-beauty-git breeze faba-icon-theme-git faba-mono-icons-git flat-remix-git halo-icons-git la-capitaine-icon-theme-git moka-icon-theme-git numix-circle-arc-icons-git numix-circle-icon-theme-git obsidian-icon-theme oranchelo-icon-theme-git paper-icon-theme papirus-icon-theme qogir-icon-theme surfn-mint-y-icons-git tela-circle-icon-theme-git vimix-icon-theme-git we10x-icon-theme-git whitesur-icon-theme-git zafiro-icon-theme
+# 166MB to download
+# 1,985 GB to install
