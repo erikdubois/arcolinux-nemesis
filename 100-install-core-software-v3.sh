126a6352a75a086fe8fdac905bc9882e719cf458
Erik Dubois <erik.dubois@gmail.com>
2022-03-04 08:07:28 +0100

    new icon theme

--- 100-install-core-software-v3.sh
+++ 100-install-core-software-v3.sh
@@ -83,6 +83,7 @@ sudo pacman -S --noconfirm --needed edu-candy-beauty-arc-mint-grey-git
 sudo pacman -S --noconfirm --needed edu-candy-beauty-arc-mint-red-git
 sudo pacman -S --noconfirm --needed edu-candy-beauty-tela-git
 sudo pacman -S --noconfirm --needed edu-papirus-dark-tela-git
+sudo pacman -S --noconfirm --needed edu-papirus-dark-tela-grey-git
 
 echo "################################################################"
 echo "################### core software installed"
