b31f9eab49c9900ba5edc1ba88aeb7f4f1d76686
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-16 11:15:16 +0100

    plasma message cone

--- Personal/935-plasma-specific-applications-and-setttings.sh
+++ Personal/935-plasma-specific-applications-and-setttings.sh
@@ -20,8 +20,9 @@
 if [ -f /usr/bin/startplasma-x11 ]; then
 	installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 	cp $installed_dir/settings/plasma/bookmarks/user-places.xbel ~/.local/share/user-places.xbel
+	echo "################################################################"
+	echo "####                 PLASMA  INSTALLED                    ######"
+	echo "################################################################"
+
 fi
 
-echo "################################################################"
-echo "####                 PLASMA  INSTALLED                    ######"
-echo "################################################################"
