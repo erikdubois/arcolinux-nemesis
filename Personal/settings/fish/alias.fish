7a4ad912800999f318437f7d0755c24ca5fae55d
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-16 20:30:33 +0100

    more personal aliases for fish

--- Personal/settings/fish/alias.fish
+++ Personal/settings/fish/alias.fish
@@ -1,1 +1,12 @@
 alias cpu="cpuid -i | grep uarch | head -n 1"
+
+#moving your personal files and folders from /personal to ~
+alias personal1='cp -Rf /personal/1/* ~'
+alias personal2='cp -Rf /personal/2/* ~'
+alias personal3='cp -Rf /personal/3/* ~'
+alias personal4='cp -Rf /personal/4/* ~'
+alias personal5='cp -Rf /personal/5/* ~'
+alias personal6='cp -Rf /personal/6/* ~'
+alias personal7='cp -Rf /personal/7/* ~'
+alias personal8='cp -Rf /personal/8/* ~'
+alias personal9='cp -Rf /personal/9/* ~'
