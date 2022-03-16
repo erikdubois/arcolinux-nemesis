29985f7ffab1a5ceda9e07f2eb247ebf50740653
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-16 14:44:39 +0100

    uninstall

--- Personal/667-uninstall-arcolinux-meta-steam.sh
+++ Personal/667-uninstall-arcolinux-meta-steam.sh
@@ -15,16 +15,30 @@
 #
 ##################################################################################################################
 
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### Uninstall steam"
+echo "################################################################"
+tput sgr0
+echo
+
 tput setaf 3;echo "  DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK."
 echo "  THIS MAY BRICK YOUR SYSTEM"
 echo "  BUT IT IS THE ONLY WAY TO GET RID OF ALL THESE PACKAGES";tput sgr0
 
-sudo pacman -Rs arcolinux-meta-steam steam lib32-libvdpau lib32-libva lib32-nvidia-utils lib32-libxtst lib32-libxrandr lib32-libpulse lib32-gdk-pixbuf2 \
-lib32-gtk2 lib32-openal lib32-mesa lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins lsof lib32-libgpg-error \
-lib32-libindicator-gtk2 lib32-libdbusmenu-glib lib32-libdbusmenu-gtk2 lib32-nss
+sudo pacman -Rs arcolinux-meta-steam-nvidia steam 
+
+#lib32-libvdpau lib32-libva lib32-nvidia-utils lib32-libxtst lib32-libxrandr lib32-libpulse lib32-gdk-pixbuf2
+#lib32-gtk2 lib32-openal lib32-mesa lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins lsof lib32-libgpg-error
+#lib32-libindicator-gtk2 lib32-libdbusmenu-glib lib32-libdbusmenu-gtk2 lib32-nss
 
 # fixing what was broken after the uninstall
-sudo pacman -S ttf-ms-fonts lsof --noconfirm
+#sudo pacman -S ttf-ms-fonts lsof --noconfirm
+echo
+tput setaf 2
 echo "################################################################"
-echo "####                      packages uninstalled            ######"
+echo "################### Steam uninstalled"
 echo "################################################################"
+tput sgr0
+echo
