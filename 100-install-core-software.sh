78b93cb8cc759d2581a851d41c8a28d7b61d0948
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-17 18:54:20 +0100

    ripgrep

--- 100-install-core-software.sh
+++ 100-install-core-software.sh
@@ -136,6 +136,7 @@ if grep -q "Arch Linux" /etc/os-release; then
   sudo pacman -S --noconfirm --needed oh-my-zsh-git
   sudo pacman -S --noconfirm --needed paru-bin
   sudo pacman -S --noconfirm --needed rate-mirrors-bin
+  sudo pacman -S --noconfirm --needed ripgrep
   sudo pacman -S --noconfirm --needed variety
   sudo pacman -S --noconfirm --needed yay-bin
   sudo pacman -S --noconfirm --needed zsh
