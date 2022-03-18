f5a61445701a68e0ee16ca9db07ae97a179da4d6
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-18 09:30:46 +0100

    test

--- 100-install-core-software.sh
+++ 100-install-core-software.sh
@@ -172,13 +172,19 @@ if grep -q "Arch Linux" /etc/os-release; then
   if [ -f /usr/share/xsessions/xfce.desktop ]; then
     sudo pacman -S --noconfirm --needed arcolinux-local-xfce4-git
     sudo pacman -S --noconfirm --needed sardi-icons
+    sudo pacman -S --noconfirm --needed thunar
+    sudo pacman -S --noconfirm --needed thunar-archive-plugin
     sudo pacman -S --noconfirm --needed arcolinux-xfce-git
   fi
 
   # when on i3
 
   if [ -f /usr/share/xsessions/i3.desktop ]; then
+    sudo pacman -S --noconfirm --needed arcolinux-i3wm-git
     sudo pacman -S --noconfirm --needed lxappearance
+    sudo pacman -S --noconfirm --needed nitrogen
+    sudo pacman -S --noconfirm --needed thunar
+    sudo pacman -S --noconfirm --needed thunar-archive-plugin    
   fi
 
 fi
