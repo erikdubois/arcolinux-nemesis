3700ea36b1bcb09433ba1b4cf4fbce4c161bed30
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-20 07:53:53 +0100

    bibata cursor

--- 100-install-core-software.sh
+++ 100-install-core-software.sh
@@ -65,6 +65,7 @@ sudo pacman -S --noconfirm --needed arcolinux-arc-themes-2021-sky-git
 sudo pacman -S --noconfirm --needed arcolinux-candy-beauty-git
 sudo pacman -S --noconfirm --needed arcolinux-fish-git
 sudo pacman -S --noconfirm --needed ayu-theme
+sudo pacman -S --noconfirm --needed bibata-cursor-theme-bin
 sudo pacman -S --noconfirm --needed chromium
 sudo pacman -S --noconfirm --needed cpuid
 sudo pacman -S --noconfirm --needed discord
