ffadf2737641ac3f6ef139e22a871aa682736661
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-24 10:48:04 +0100

    config dev

--- 100-install-core-software.sh
+++ 100-install-core-software.sh
@@ -221,7 +221,8 @@ if grep -q "Arch Linux" /etc/os-release; then
   sudo pacman -S --noconfirm --needed arcolinux-logout-git
   sudo pacman -S --noconfirm --needed arcolinux-paru-git
   sudo pacman -S --noconfirm --needed arcolinux-root-git
-  sudo pacman -S --noconfirm --needed arcolinux-system-config-git
+  #sudo pacman -S --noconfirm --needed arcolinux-system-config-git
+  sudo pacman -S --noconfirm --needed arcolinux-system-config-dev-git  
   #sudo pacman -S --noconfirm --needed lsb-release
   sudo pacman -S --noconfirm --needed arcolinux-tweak-tool-git
   sudo pacman -S --noconfirm --needed arcolinux-variety-git
