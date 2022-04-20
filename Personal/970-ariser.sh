1ea09c8426ee644266f8d26cec9af5a953cf4eaf
Erik Dubois <erik.dubois@gmail.com>
2022-04-20 21:31:48 +0200

    more packages for ariser

--- Personal/970-ariser.sh
+++ Personal/970-ariser.sh
@@ -17,15 +17,50 @@
 
 installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 
+func_install() {
+    if pacman -Qi $1 &> /dev/null; then
+        tput setaf 2
+        echo "###############################################################################"
+        echo "################## The package "$1" is already installed"
+        echo "###############################################################################"
+        echo
+        tput sgr0
+    else
+        tput setaf 3
+        echo "###############################################################################"
+        echo "##################  Installing package "  $1
+        echo "###############################################################################"
+        echo
+        tput sgr0
+        sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
 if [ -f /usr/local/bin/get-nemesis-on-ariser ]; then
 
 	echo
 	tput setaf 2
 	echo "################################################################"
-	echo "################### We are on a AA iso"
+	echo "################### We are on ARISER iso"
 	echo "################################################################"
 	tput sgr0
 	echo
+
+	list=(
+	arcolinux-gtk3-arcolinux-candy-beauty-git
+	arcolinux-kvantum-git
+	arcolinux-qt5-git
+	kvantum
+	qt5ct
+	)
+
+	count=0
+
+	for name in "${list[@]}" ; do
+	    count=$[count+1]
+	    tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	    func_install $name
+	done
 
 	if [ -f /usr/share/xsessions/xfce.desktop ]; then
 		echo
