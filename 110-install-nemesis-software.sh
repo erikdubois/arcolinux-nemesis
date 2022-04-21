46ca0449acc208f8eda3ac28e7cac98c2a93461b
Erik Dubois <erik.dubois@gmail.com>
2022-04-21 07:50:19 +0200

    vimix out

--- 110-install-nemesis-software.sh
+++ 110-install-nemesis-software.sh
@@ -64,4 +64,4 @@ sudo pacman -S --noconfirm --needed edu-candy-beauty-arc-mint-red-git
 sudo pacman -S --noconfirm --needed edu-candy-beauty-tela-git
 sudo pacman -S --noconfirm --needed edu-papirus-dark-tela-git
 sudo pacman -S --noconfirm --needed edu-papirus-dark-tela-grey-git
-sudo pacman -S --noconfirm --needed edu-vimix-dark-tela-git
+#sudo pacman -S --noconfirm --needed edu-vimix-dark-tela-git
