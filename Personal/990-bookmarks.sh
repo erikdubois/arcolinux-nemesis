e35334340a7549b2edd9d8d9b80fc8517da30898
Erik Dubois <erik.dubois@gmail.com>
2022-04-23 11:09:44 +0200

    update

--- Personal/990-bookmarks.sh
+++ Personal/990-bookmarks.sh
@@ -26,41 +26,10 @@
 #tput setaf 8 = light blue
 ##################################################################################################################
 
-echo
-tput setaf 3
-echo "################################################################"
-echo "################### Start"
-echo "################################################################"
-tput sgr0
-echo
-
-# Problem solving commands
-
-# Read before using it.
-# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
-# git reset --hard orgin/master
-# ONLY if you are very sure and no coworkers are on your github.
-
-# Command that have helped in the past
-# Force git to overwrite local files on pull - no merge
-# git fetch all
-# git push --set-upstream origin master
-# git reset --hard orgin/master
-
-
-#setting up git
-#https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
-#https://medium.com/clarusway/how-to-use-git-github-without-asking-for-authentication-always-passwordless-usage-of-private-git-8c32489bc2e9
-#https://blog.nillsf.com/index.php/2021/05/27/github-sso-using-password-protected-ssh-keys
-
-project=$(basename `pwd`)
-githubdir="arcolinux"
-echo "-----------------------------------------------------------------------------"
-echo "this is project https://github.com/$githubdir/$project"
-echo "-----------------------------------------------------------------------------"
+installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 
 echo
-tput setaf 1
+tput setaf 2
 echo "################################################################"
 echo "#####  Choose wisely - one time setup after clean install   ####"
 echo "################################################################"
