9b386d081d9f281287b9a78cf5589be3a0ce906a
Erik Dubois <erik.dubois@gmail.com>
2022-05-31 20:11:27 +0200

    sound configs

--- AUR/install-pipewire-v1.sh
+++ AUR/install-pipewire-v1.sh
@@ -19,37 +19,29 @@
 #starting on an ArcoLinuxL iso
 #https://wiki.archlinux.org/title/PipeWire#Bluetooth_devices
 
+#compare
+
+sudo pacman -R --noconfirm gnome-bluetooth blueberry
+#sudo pacman -R --noconfirm pipewire-pulse
+#sudo pacman -R --noconfirm pipewire-alsa
+#sudo pacman -Rdd --noconfirm pipewire-jack
+#sudo pacman -R --noconfirm pipewire-media-session
+#sudo pacman -R --noconfirm pipewire-zeroconf
+#sudo pacman -Rdd --noconfirm pipewire
+
 sudo pacman -S --noconfirm --needed pipewire
 sudo pacman -S --noconfirm --needed pipewire-media-session
 sudo pacman -S --noconfirm --needed pipewire-alsa
 sudo pacman -Rdd --noconfirm jack2
-
-# : removing jack2 breaks dependency 'jack' required by ffmpeg
-# :: removing jack2 breaks dependency 'jack' required by ffmpeg4.4
-# :: removing jack2 breaks dependency 'libjack.so=0-64' required by fluidsynth
-# :: removing jack2 breaks dependency 'libjack.so=0-64' required by gst-plugins-good
-# :: removing jack2 breaks dependency 'libjack.so=0-64' required by portaudio
-# :: removing jack2 breaks dependency 'jack' required by simplescreenrecorder
-
 sudo pacman -S --noconfirm --needed pipewire-jack
 sudo pacman -S --noconfirm --needed pipewire-zeroconf
 
-
-
- 
-sudo pacman -R --noconfirm pulseaudio-equalizer-ladspa
 sudo pacman -R --noconfirm pulseaudio-alsa
-sudo pacman -R --noconfirm gnome-bluetooth blueberry
 sudo pacman -R --noconfirm pulseaudio-bluetooth
 sudo pacman -R --noconfirm pulseaudio
-
-
-
 sudo pacman -S --noconfirm --needed pipewire-pulse
-sudo pacman -S --noconfirm --needed blueberry
-sudo pacman -S --noconfirm --needed pavucontrol
-sudo systemctl enable bluetooth.service
 
-exit 1
+sudo pacman -S --noconfirm --needed gnome-bluetooth blueberry
+sudo systemctl enable bluetooth.service
 
 echo "Reboot now"
--- AUR/install-pulseaudio-v1.sh
+++ AUR/install-pulseaudio-v1.sh
@@ -19,22 +19,29 @@
 #starting on an ArcoLinuxL iso
 #https://wiki.archlinux.org/title/PipeWire#Bluetooth_devices
 
+#compare
+
 sudo pacman -R --noconfirm gnome-bluetooth blueberry
 sudo pacman -R --noconfirm pipewire-pulse
 sudo pacman -R --noconfirm pipewire-alsa
 sudo pacman -Rdd --noconfirm pipewire-jack
 sudo pacman -R --noconfirm pipewire-media-session
 sudo pacman -R --noconfirm pipewire-zeroconf
-
-
-
 sudo pacman -Rdd --noconfirm pipewire
 
+#sudo pacman -S --noconfirm --needed pipewire
+#sudo pacman -S --noconfirm --needed pipewire-media-session
+#sudo pacman -S --noconfirm --needed pipewire-alsa
+#sudo pacman -Rdd --noconfirm jack2
+#do pacman -S --noconfirm --needed pipewire-jack
+#udo pacman -S --noconfirm --needed pipewire-zeroconf
 
-sudo pacman -S --noconfirm --needed pulseaudio-equalizer-ladspa
 sudo pacman -S --noconfirm --needed pulseaudio-alsa
 sudo pacman -S --noconfirm --needed pulseaudio-bluetooth
 sudo pacman -S --noconfirm --needed pulseaudio
+sudo pacman -S --noconfirm --needed jack2
+
 sudo pacman -S --noconfirm --needed gnome-bluetooth blueberry
+sudo systemctl enable bluetooth.service
 
 echo "Reboot now"
