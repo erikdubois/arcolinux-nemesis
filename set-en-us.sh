8c70ad0c9ffea310eb073f640ebcbe7bb1c53124
arcolinuxz <arcolinuxinfo@gmail.com>
2022-07-01 11:46:57 +0200

    en_US

---
+++ set-en-us.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+sudo localectl set-locale LANG=en_US.UTF-8
+
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### Done"
+echo "################################################################"
+tput sgr0
+echo
+
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### rebooting now after 5 seconds"
+echo "Press CTRL +C to stop script"
+echo "################################################################"
+tput sgr0
+echo
+
+sleep 5
+
+sudo reboot
