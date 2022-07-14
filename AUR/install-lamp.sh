bd8afe8a0f6d468666f449e823760b1e031298b8
arcolinuxz <arcolinuxinfo@gmail.com>
2022-07-14 16:05:50 +0200

    update

--- AUR/install-lamp.sh
+++ AUR/install-lamp.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 #set -e
 ##################################################################################################################
-# Author	:	Erik Dubois
-# Website	:	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxb.com
-# Website	:	https://www.arcolinuxiso.com
-# Website	:	https://www.arcolinuxforum.com
+# Author  : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website : https://www.arcolinux.info
+# Website : https://www.arcolinux.com
+# Website : https://www.arcolinuxd.com
+# Website : https://www.arcolinuxb.com
+# Website : https://www.arcolinuxiso.com
+# Website : https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
 
 # url : https://www.linuxshelltips.com/install-lamp-archlinux/
 #https://wiki.archlinux.org/title/Wordpress
 
-website="wordpress"
+tput setaf 1
+echo "###################################################################################################"
+echo "WORK IN PROGRESS"
+echo "THIS IS NOT WORKING YET"
+echo "###################################################################################################"
+tput sgr0
 
 sudo pacman -Rs php apache php-apache phpmyadmin mariadb --noconfirm
 
