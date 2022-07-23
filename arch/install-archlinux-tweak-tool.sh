c9d046bb7660e65cbee5563517515ba5ad917e11
arcolinuxz <arcolinuxinfo@gmail.com>
2022-07-23 15:37:43 +0200

    get att on Arch

---
+++ arch/install-archlinux-tweak-tool.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website	: https://www.arcolinux.info
+# Website	: https://www.arcolinux.com
+# Website	: https://www.arcolinuxd.com
+# Website	: https://www.arcolinuxb.com
+# Website	: https://www.arcolinuxiso.com
+# Website	: https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black 
+#tput setaf 1 = red 
+#tput setaf 2 = green
+#tput setaf 3 = yellow 
+#tput setaf 4 = dark blue 
+#tput setaf 5 = purple
+#tput setaf 6 = cyan 
+#tput setaf 7 = gray 
+#tput setaf 8 = light blue
+##################################################################################################################
+
+git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
+cd /tmp/yay-bin
+makepkg
+sudo pacman -U yay-bin*.zst
+yay -S archlinux-tweak-tool-git --noconfirm
+
+tput setaf 2
+echo "##################################################################################"
+echo "###################                   DONE                    ####################"
+echo "##################################################################################"
+tput sgr0
+
+
+
