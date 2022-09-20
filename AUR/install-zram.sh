3a54376a769db6bcef16cf0ec8b820c27e60c84b
Erik Dubois <erik.dubois@gmail.com>
2022-09-20 07:26:30 +0200

    zram

---
+++ AUR/install-zram.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#https://github.com/systemd/zram-generator
+
+sudo pacman -S zram-generator --noconfirm --needed
+
+#----------------------------------------------------------------------------------
+
+echo '[zram0]
+zram-size = ram / 2' | sudo tee /etc/systemd/zram-generator.conf
+
+#create new devices
+sudo systemctl daemon-reload
+
+sudo systemctl start /dev/zram0
+
+echo "check with"
+echo "swapon or zramctl or"
+echo "with systemdsystemctl status systemd-zram-setup@zram0.service"
