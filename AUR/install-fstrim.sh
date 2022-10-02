4159f919f70031f1cc81ef87eb61d65839e5e277
arcolinuxz <arcolinuxinfo@gmail.com>
2022-10-02 07:51:36 +0200

    fstrim

---
+++ AUR/install-fstrim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#https://wiki.archlinux.org/title/Solid_state_drive#TRIM
+
+sudo systemctl enable fstrim.timer
