2fd256f8035e0acc19e326e1b2f462f7728f26e8
arcolinuxz <arcolinuxinfo@gmail.com>
2022-12-23 19:38:01 +0100

    upate

--- Personal/970-arco.sh
+++ Personal/970-arco.sh
@@ -62,7 +62,7 @@ if [ -f /usr/local/bin/get-nemesis-on-arcolinux ]; then
 		sudo sed -i "s/$FIND/$REPLACE/g" /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
 
 		FIND="Sardi-Arc"
-		REPLACE="Edu-Papirus-Dark-Tela"
+		REPLACE="arcolinux-candy-beauty"
 		sed -i "s/$FIND/$REPLACE/g" ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
 		sudo sed -i "s/$FIND/$REPLACE/g" /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
 
