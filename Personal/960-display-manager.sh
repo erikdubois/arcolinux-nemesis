f7f4866f2511ed5f778884ae9dded722b29dcf8d
arcolinuxz <arcolinuxinfo@gmail.com>
2022-12-24 09:07:39 +0100

    update

--- Personal/960-display-manager.sh
+++ Personal/960-display-manager.sh
@@ -76,7 +76,7 @@ fi
 
 # we are on Arch Linux
 
-if [ -f /usr/local/bin/get-nemesis-on-arch ]; then
+if grep -q "archlinux" /etc/os-release; then
 
 	echo
 	tput setaf 2
@@ -87,12 +87,48 @@ if [ -f /usr/local/bin/get-nemesis-on-arch ]; then
 	echo
 
 	echo
-	echo "Changing sddm theme"
+	echo "Installing and changing sddm theme"
+	echo "Archinstall is by default lightdm"
 	echo
 	sudo pacman -S --noconfirm --needed arcolinux-sddm-simplicity-git
 	FIND="Current=breeze"
 	REPLACE="Current=arcolinux-simplicity"
 	sudo sed -i "s/$FIND/$REPLACE/g" /etc/sddm.conf
+
+	if [ -f /etc/lightdm/lightdm.conf ]; then
+
+		echo
+		echo "Autologin to lightdm"
+		echo
+		FIND="#autologin-user="
+		REPLACE="autologin-user=$USER"
+    	sudo sed -i "s/$FIND/$REPLACE/g" /etc/lightdm/lightdm.conf
+
+		FIND="#autologin-session="
+		REPLACE="autlogin-session=xfce"
+    	sudo sed -i "s/$FIND/$REPLACE/g" /etc/lightdm/lightdm.conf
+
+		sudo usermod -a -G autologin $USER
+
+	fi
+
+	if [ -f /etc/lightdm/lightdm-gtk-greeter.conf ]; then
+
+		echo
+		echo "Changing the look of lightdm gtk greeter"
+		echo
+
+		FIND="#theme-name="
+		REPLACE="theme-name=Arc-Dark"
+		sudo sed -i "s/$FIND/$REPLACE/g" /etc/lightdm/lightdm-gtk-greeter.conf
+
+		sudo cp $installed_dir/settings/wallpaper/lightdm.jpg /etc/lightdm/lightdm.jpg
+
+		FIND="#background="
+		REPLACE="background=\/etc\/lightdm\/lightdm.jpg"
+		sudo sed -i "s/$FIND/$REPLACE/g" /etc/lightdm/lightdm-gtk-greeter.conf
+
+	fi
 
 	echo
 	tput setaf 6
