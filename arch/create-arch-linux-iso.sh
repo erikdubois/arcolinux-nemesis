d3417d2383bce70f9831aca85bcdfe38d8265419
Erik Dubois <erik.dubois@gmail.com>
2022-12-31 10:01:09 +0100

    archiso

---
+++ arch/create-arch-linux-iso.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+######################################################################################################################
+
+sudo pacman -S archiso --noconfirm --needed
+
+sudo mkarchiso -v /usr/share/archiso/configs/releng/
