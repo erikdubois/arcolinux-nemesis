69d7c35893606a509c8db7838fdec3b07772004d
Erik Dubois <erik.dubois@gmail.com>
2023-01-03 09:53:57 +0100

    wf-recorder

--- Personal/970-arco.sh
+++ Personal/970-arco.sh
@@ -74,6 +74,39 @@ if grep -q "ArcoLinux" /etc/os-release; then
 
 	fi
 
+	if [ -f /usr/share/wayland-sessions/sway.desktop ]; then
+		echo
+		tput setaf 2
+		echo "################################################################"
+		echo "################### We are on Sway"
+		echo "################################################################"
+		tput sgr0
+		echo
+		
+		echo
+		echo "Installing extra packages"
+		echo
+
+		sudo pacman -S --noconfirm --needed wf-recorder
+
+	fi
+
+	if [ -f /usr/share/wayland-sessions/hyprland.desktop ]; then
+		echo
+		tput setaf 2
+		echo "################################################################"
+		echo "################### We are on Hyprland"
+		echo "################################################################"
+		tput sgr0
+		echo
+		
+		echo
+		echo "Installing extra packages"
+		echo
+
+		sudo pacman -S --noconfirm --needed wf-recorder
+
+	fi
 
 	echo
 	echo "ArchLinux Logout - handy icons"
