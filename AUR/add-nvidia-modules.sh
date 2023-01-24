56df6e08a58b252b05304969e9d072d6732f88ab
arcolinuxz <arcolinuxinfo@gmail.com>
2023-01-24 17:05:35 +0100

    update

--- AUR/add-nvidia-modules.sh
+++ AUR/add-nvidia-modules.sh
@@ -32,11 +32,43 @@ installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 ##################################################################################################################
 
 # https://wiki.hyprland.org/Nvidia/
+# https://community.kde.org/Plasma/Wayland/Nvidia
 
 echo
 tput setaf 2
 echo "################################################################"
-echo "################### Adding nvidia modules for WAYLAND and NVIDIA"
+echo "################### Making sure nvidia-dkms is installed or else exit"
+echo "################################################################"
+tput sgr0
+echo
+
+# Just checking if nvidia-dkms is installed else stop
+if pacman -Qi nvidia-dkms &> /dev/null; then
+
+	tput setaf 2
+	echo "################################################################"
+	echo "#########  Checking ..."$package" is installed... we can continue"
+	echo "################################################################"
+	tput sgr0
+
+else
+
+	tput setaf 1
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  Nvidia-dkms has NOT been installed"
+	echo "!!!!!!!!!  Script can not continue"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	tput sgr0
+	exit 1
+
+fi
+
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### Adding nvidia modules for Wayland and Nvidia"
+echo "################### MODULES= nvidia nvidia_modeset nvidia_uvm nvidia_drm"
+echo "################### in /etc/mkinitcpio.conf"
 echo "################################################################"
 tput sgr0
 echo
@@ -47,6 +79,28 @@ sudo sed -i "s/$FIND/$REPLACE/g" /etc/mkinitcpio.conf
 
 sudo mkinitcpio -P
 
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### Adding option nvidia-drm.modeset=1"
+echo "################### to the kernel"
+echo "################################################################"
+tput sgr0
+echo
+
+echo 'option nvidia-drm.modeset=1' >> /etc/modprobe.d/arco-wayland-nvidia.conf
+
+
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### Removing sddm and installing sddm-git"
+echo "################################################################"
+tput sgr0
+echo
+
+sudo pacman -Rdd --noconfirm sddm
+sudo pacman -S --noconfirm sddm-git
 echo
 tput setaf 6
 echo "################################################################"
