88a246ca9a7856ab7435fc0ee5ba50d08033680c
arcolinuxz <arcolinuxinfo@gmail.com>
2023-03-03 08:16:48 +0100

    streamdeck-ui

---
+++ AUR/install-streamdeck-ui.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# https://github.com/timothycrosley/streamdeck-ui/blob/master/docs/installation/arch.md
+
+package="hidapi"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+		tput setaf 2
+		echo "################################################################"
+		echo "################## "$package" is already installed"
+		echo "################################################################"
+		tput sgr0
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi yay &> /dev/null; then
+
+		tput setaf 3
+		echo "################################################################"
+		echo "######### Installing with yay"
+		echo "################################################################"
+		tput sgr0
+
+		yay -S --noconfirm $package
+
+	elif pacman -Qi paru &> /dev/null; then
+
+		tput setaf 3
+		echo "################################################################"
+		echo "######### Installing with paru"
+		echo "################################################################"
+		tput sgr0
+		paru -S --noconfirm --needed $package
+
+	fi
+
+fi
+
+# Just checking if installation was successful
+if pacman -Qi $package &> /dev/null; then
+
+	tput setaf 2
+	echo "################################################################"
+	echo "#########  Checking ..."$package" has been installed"
+	echo "################################################################"
+	tput sgr0
+
+else
+
+	tput setaf 1
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	tput sgr0
+
+fi
+
+#----------------------------------------------------------------------------------
+
+package="python-pip"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+		tput setaf 2
+		echo "################################################################"
+		echo "################## "$package" is already installed"
+		echo "################################################################"
+		tput sgr0
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi yay &> /dev/null; then
+
+		tput setaf 3
+		echo "################################################################"
+		echo "######### Installing with yay"
+		echo "################################################################"
+		tput sgr0
+
+		yay -S --noconfirm $package
+
+	elif pacman -Qi paru &> /dev/null; then
+
+		tput setaf 3
+		echo "################################################################"
+		echo "######### Installing with paru"
+		echo "################################################################"
+		tput sgr0
+		paru -S --noconfirm --needed $package
+
+	fi
+
+fi
+
+
+# Just checking if installation was successful
+if pacman -Qi $package &> /dev/null; then
+
+	tput setaf 2
+	echo "################################################################"
+	echo "#########  Checking ..."$package" has been installed"
+	echo "################################################################"
+	tput sgr0
+
+else
+
+	tput setaf 1
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	tput sgr0
+
+fi
+
+#----------------------------------------------------------------------------------
+
+package="qt6-base"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+		tput setaf 2
+		echo "################################################################"
+		echo "################## "$package" is already installed"
+		echo "################################################################"
+		tput sgr0
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi yay &> /dev/null; then
+
+		tput setaf 3
+		echo "################################################################"
+		echo "######### Installing with yay"
+		echo "################################################################"
+		tput sgr0
+
+		yay -S --noconfirm $package
+
+	elif pacman -Qi paru &> /dev/null; then
+
+		tput setaf 3
+		echo "################################################################"
+		echo "######### Installing with paru"
+		echo "################################################################"
+		tput sgr0
+		paru -S --noconfirm --needed $package
+
+	fi
+
+fi
+
+
+# Just checking if installation was successful
+if pacman -Qi $package &> /dev/null; then
+
+	tput setaf 2
+	echo "################################################################"
+	echo "#########  Checking ..."$package" has been installed"
+	echo "################################################################"
+	tput sgr0
+
+else
+
+	tput setaf 1
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!!!!!!!!  "$package" has NOT been installed"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	tput sgr0
+
+fi
+
+#----------------------------------------------------------------------------------
+
+PATH=$PATH:$HOME/.local/bin
+
+python -m pip install --upgrade pip
+python -m pip install setuptools
+
+sudo sh -c 'echo "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"0fd9\", TAG+=\"uaccess\"" > /etc/udev/rules.d/70-streamdeck.rules'
+
+sudo udevadm trigger
+
+python -m pip install streamdeck-ui --user
