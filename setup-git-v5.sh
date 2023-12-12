3ad661d676804c6c2ddf6e35615255b39df016f0
Erik Dubois <erik.dubois@gmail.com>
2023-12-12 13:46:18 +0100

    push to all personal githubs

--- setup-git-v5.sh
+++ setup-git-v5.sh
@@ -1,39 +1,15 @@
 #!/bin/bash
-#set -e
+#
 ##################################################################################################################
-# Author    : Erik Dubois
-# Website   : https://www.erikdubois.be
-# Website   : https://www.alci.online
-# Website   : https://www.ariser.eu
-# Website   : https://www.arcolinux.info
-# Website   : https://www.arcolinux.com
-# Website   : https://www.arcolinuxd.com
-# Website   : https://www.arcolinuxb.com
-# Website   : https://www.arcolinuxiso.com
-# Website   : https://www.arcolinuxforum.com
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-#tput setaf 0 = black
-#tput setaf 1 = red
-#tput setaf 2 = green
-#tput setaf 3 = yellow
-#tput setaf 4 = dark blue
-#tput setaf 5 = purple
-#tput setaf 6 = cyan
-#tput setaf 7 = gray
-#tput setaf 8 = light blue
-##################################################################################################################
-
-echo
-tput setaf 3
-echo "################################################################"
-echo "################### Start"
-echo "################################################################"
-tput sgr0
-echo
 
 # Problem solving commands
 
@@ -48,32 +24,22 @@ echo
 # git push --set-upstream origin master
 # git reset --hard orgin/master
 
-
-#setting up git
-#https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
-#https://medium.com/clarusway/how-to-use-git-github-without-asking-for-authentication-always-passwordless-usage-of-private-git-8c32489bc2e9
-#https://blog.nillsf.com/index.php/2021/05/27/github-sso-using-password-protected-ssh-keys
-
 project=$(basename `pwd`)
-#githubdir=$(basename $(dirname "$PWD" | tr '[:upper:]' '[:lower:]'))
 echo "-----------------------------------------------------------------------------"
 echo "this is project https://github.com/erikdubois/"$project
 echo "-----------------------------------------------------------------------------"
-
 git config --global pull.rebase false
-git config --global push.default simple
 git config --global user.name "Erik Dubois"
 git config --global user.email "erik.dubois@gmail.com"
 sudo git config --system core.editor nano
 #git config --global credential.helper cache
 #git config --global credential.helper 'cache --timeout=32000'
+git config --global push.default simple
 
 git remote set-url origin git@github.com-edu:erikdubois/$project
 
-echo
-tput setaf 3
+echo "Everything set"
+
 echo "################################################################"
-echo "################### End"
+echo "###################    T H E   E N D      ######################"
 echo "################################################################"
-tput sgr0
-echo
