362f79060ddbd5dab1a553e6299692152a90d7b5
Erik Dubois <erik.dubois@gmail.com>
2024-01-27 14:30:07 +0100

    alacritty tranparent

--- Personal/970-archbang.sh
+++ Personal/970-archbang.sh
@@ -47,6 +47,9 @@ if grep -q "ArchBang" /etc/os-release; then
 	echo "Variety conf ArcoLinux"
 	sudo pacman -S --noconfirm --needed arcolinux-variety-git
 	sudo pacman -S --noconfirm --needed arcolinux-variety-autostart-git
+	
+	echo "Alacritty conf ArcoLinux"
+	sudo pacman -S --noconfirm --needed arcolinux-alacritty-git
 
 	echo
 	echo "Adding nanorc"
