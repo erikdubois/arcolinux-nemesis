635529ad36400fe4f5eb91c17a177a7b57242d78
arcolinuxz <arcolinuxinfo@gmail.com>
2024-03-13 06:38:00 +0100

    back in after wrong merge

--- arch/get-the-keys-and-repos.sh
+++ arch/get-the-keys-and-repos.sh
@@ -3,19 +3,18 @@
 ######################################################################################################################
 
 sudo pacman -S wget --noconfirm --needed
-sudo pacman -S jq --noconfirm --needed
 
 echo "Getting the ArcoLinux keys from the ArcoLinux repo"
 
-sudo wget "$(echo "$response" | jq -r '[.[] | select(.name | contains("arcolinux-keyring")) | .name] | .[0] | sub("arcolinux-keyring-"; "https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-")')" -O /tmp/arcolinux-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed /tmp/arcolinux-any.pkg.tar.zst
+sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-20251209-3-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
+sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
 
 ######################################################################################################################
 
 echo "Getting the latest arcolinux mirrors file"
 
-sudo wget "$(echo "$response" | jq -r '[.[] | select(.name | contains("arcolinux-mirrorlist-git-")) | .name] | .[0] | sub("arcolinux-mirrorlist-git-"; "https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-")')" -O /tmp/arcolinux-mirrorlist-git-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-any.pkg.tar.zst
+sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-24.03-12-any.pkg.tar.zst -O /tmp/arcolinux-mirrorlist-git-24.03-12-any.pkg.tar.zst
+sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-24.03-12-any.pkg.tar.zst
 echo '
 
 #[arcolinux_repo_testing]
