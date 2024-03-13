bbc6d1386519cabfde7fd2a8c746caf4dc5f1df4
nazDridoy <45564804+nazdridoy@users.noreply.github.com>
2024-03-13 14:08:57 +0600

    sync arcolinux_repo db

--- arch/get-the-keys-and-repos.sh
+++ arch/get-the-keys-and-repos.sh
@@ -4,17 +4,17 @@
 
 sudo pacman -S wget --noconfirm --needed
 sudo pacman -S jq --noconfirm --needed
-
+arco_repo_db=$(wget -qO- https://api.github.com/repos/arcolinux/arcolinux_repo/contents/x86_64)
 echo "Getting the ArcoLinux keys from the ArcoLinux repo"
 
-sudo wget "$(echo "$response" | jq -r '[.[] | select(.name | contains("arcolinux-keyring")) | .name] | .[0] | sub("arcolinux-keyring-"; "https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-")')" -O /tmp/arcolinux-any.pkg.tar.zst
+sudo wget "$(echo "$arco_repo_db" | jq -r '[.[] | select(.name | contains("arcolinux-keyring")) | .name] | .[0] | sub("arcolinux-keyring-"; "https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-")')" -O /tmp/arcolinux-any.pkg.tar.zst
 sudo pacman -U --noconfirm --needed /tmp/arcolinux-any.pkg.tar.zst
 
 ######################################################################################################################
 
 echo "Getting the latest arcolinux mirrors file"
 
-sudo wget "$(echo "$response" | jq -r '[.[] | select(.name | contains("arcolinux-mirrorlist-git-")) | .name] | .[0] | sub("arcolinux-mirrorlist-git-"; "https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-")')" -O /tmp/arcolinux-mirrorlist-git-any.pkg.tar.zst
+sudo wget "$(echo "$arco_repo_db" | jq -r '[.[] | select(.name | contains("arcolinux-mirrorlist-git-")) | .name] | .[0] | sub("arcolinux-mirrorlist-git-"; "https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-")')" -O /tmp/arcolinux-mirrorlist-git-any.pkg.tar.zst
 sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-any.pkg.tar.zst
 echo '
 
