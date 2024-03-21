3e6ff8b943e50da4b68ab0cfc72c2f03d151b645
arcolinuxz <arcolinuxinfo@gmail.com>
2024-03-21 07:55:13 +0100

    kwinpolonium

--- 500-plasma-6.sh
+++ 500-plasma-6.sh
@@ -88,6 +88,7 @@ if [ -f /usr/share/wayland-sessions/plasma.desktop ]; then
   sudo pacman -S --noconfirm --needed thunar-archive-plugin
   sudo pacman -S --noconfirm --needed meld
   sudo pacman -S --noconfirm --needed kio-admin
+  sudo pacman -S --noconfirm --needed kwin-polonium
   sudo pacman -S --noconfirm --needed ripgrep
   sudo pacman -S --noconfirm --needed sublime-text-4
   sudo pacman -S --noconfirm --needed the_platinum_searcher-bin
