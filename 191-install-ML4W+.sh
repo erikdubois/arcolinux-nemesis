43f863932014a582b8de725a41e29a3feaf2f61f
arcolinuxz <arcolinuxinfo@gmail.com>
2024-05-16 15:24:10 +0200

    update

---
+++ 191-install-ML4W+.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
+
+##################################################################################################################
+
+# https://www.youtube.com/watch?v=HMxHUvN6VGo
+# https://gitlab.com/stephan-raabe/
+cp /etc/skel/.bashrc $HOME/dotfiles/.bashrc-personal
+
+if ! grep -q "~/dotfiles/.bashrc-personal" $HOME/.bashrc; then
+	echo "
+[[ -f ~/dotfiles/.bashrc-personal ]] && . ~/dotfiles/.bashrc-personal" | tee -a $HOME/dotfiles/.bashrc
+	source  ~/.bashrc
+fi
+
+if ! grep -q "# Keybindings ArcoLinux" $HOME/dotfiles/hypr/conf/keybindings/default.conf; then
+
+echo "
+
+# Keybindings ArcoLinux
+bind = $mainMod SHIFT, Return, exec, ~/dotfiles/.settings/filemanager.sh
+bind = $mainMod SHIFT, Q, killactive
+bind = $mainMod, X, exec, archlinux-logout
+bind = $mainMod SHIFT, X, exec, arcolinux-powermenu
+bind = , F12, exec, xfce4-terminal --drop-down
+bind = CTRL ALT, T, exec, kitty
+bind = CTRL ALT, Return, exec, kitty
+bind = $mainMod , D, exec, rofi -show drun -replace -i
+bind = $mainMod SHIFT, D, exec, rofi -show drun -replace -i" | tee -a $HOME/dotfiles/hypr/conf/keybindings/default.conf
+fi
