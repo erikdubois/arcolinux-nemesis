f73edcd49e5a8635a624632001ee738913d63205
arcolinuxz <arcolinuxinfo@gmail.com>
2024-05-29 10:53:11 +0200

    qt6ct out

--- Personal/970-manjaro.sh
+++ Personal/970-manjaro.sh
@@ -69,6 +69,7 @@ if grep -q "Manjaro" /etc/os-release; then
 
 	sudo pacman -R --noconfirm sardi-icons
 	sudo pacman -R --noconfirm qt5ct
+	sudo pacman -R --noconfirm qt6ct
   	sudo pacman -R --noconfirm surfn-icons-git
   	sudo pacman -S --noconfirm --needed surfn-plasma-dark-icons-git 
   	sudo pacman -S --noconfirm --needed arcolinux-meta-plasma-design
