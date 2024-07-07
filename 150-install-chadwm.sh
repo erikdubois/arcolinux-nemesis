4a9b7ea752e96e2de8663a4482328cb1d435cabc
arcolinuxz <arcolinuxinfo@gmail.com>
2024-07-07 14:42:19 +0200

    erik

--- 150-install-chadwm.sh
+++ 150-install-chadwm.sh
@@ -77,6 +77,7 @@ func_install_chadwm() {
     archlinux-logout-git
     arcolinux-chadwm-git
     arcolinux-chadwm-pacman-hook-git
+    arcolinux-nlogout-git
     arcolinux-powermenu-git
     arcolinux-wallpapers-candy-git
     arcolinux-wallpapers-git
