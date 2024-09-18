9a18974562f5309b2d6b4f5bef943c921b22d97d
arcolinuxz <arcolinuxinfo@gmail.com>
2024-09-18 12:12:17 +0200

    typo

--- Personal/settings/ubuntu-chadwm/install-chadwm-on-ubuntu.sh
+++ Personal/settings/ubuntu-chadwm/install-chadwm-on-ubuntu.sh
@@ -72,7 +72,7 @@ sudo apt install -y variety
 
 git clone https://github.com/arcolinux/arcolinux-powermenu  /tmp/arcolinux-powermenu
 
-sudo cp /tmp/arcolinux-powermenu/usr/local/bin/arcoliunux-powermenu /usr/local/bin
+sudo cp /tmp/arcolinux-powermenu/usr/local/bin/arcolinux-powermenu /usr/local/bin
 cp -r /tmp/arcolinux-powermenu/etc/skel/.bin ~
 cp -r /tmp/arcolinux-powermenu/etc/skel/.config ~
 
