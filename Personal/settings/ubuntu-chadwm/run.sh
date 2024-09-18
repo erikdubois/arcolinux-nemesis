085ae718ae18b8c807928ac43a9a89f4ec86aad9
arcolinuxz <arcolinuxinfo@gmail.com>
2024-09-18 13:17:47 +0200

    picom

--- Personal/settings/ubuntu-chadwm/run.sh
+++ Personal/settings/ubuntu-chadwm/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-#picom -b  --config ~/.config/arco-chadwm/picom/picom.conf &
-#picom &
+picom -b  --config ~/.config/arco-chadwm/picom/picom.conf &
+
 run "numlockx on"
 sxhkd -c ~/.config/arco-chadwm/sxhkd/sxhkdrc &
 
