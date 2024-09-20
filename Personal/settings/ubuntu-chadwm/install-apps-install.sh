3abee6cb5f49656abed020d8443c53fd9e522e54
arcolinuxz <arcolinuxinfo@gmail.com>
2024-09-20 12:20:24 +0200

    gnome-software

--- Personal/settings/ubuntu-chadwm/install-apps-install.sh
+++ Personal/settings/ubuntu-chadwm/install-apps-install.sh
@@ -56,6 +56,7 @@ sudo apt install -y fonts-noto
 sudo apt install -y fonts-roboto
 sudo apt install -y gimp
 sudo apt install -y gnome-bluetooth
+sudo apt install -y gnome-software
 sudo apt install -y gnome-tweaks
 sudo apt install -y hardinfo
 sudo apt install -y hw-probe
