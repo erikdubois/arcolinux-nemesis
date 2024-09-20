b9f120108081197a956b5b598feecceb20c37027
arcolinuxz <arcolinuxinfo@gmail.com>
2024-09-20 12:33:06 +0200

    sh added for bar

--- Personal/settings/ubuntu-chadwm/run.sh
+++ Personal/settings/ubuntu-chadwm/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+sxhkd -c ~/.config/arco-chadwm/sxhkd/sxhkdrc &
+
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 picom -b  --config ~/.config/arco-chadwm/picom/picom.conf &
 variety -n &
 numlockx on &
-sxhkd -c ~/.config/arco-chadwm/sxhkd/sxhkdrc &
-
 pkill bar.sh &
-~/.config/arco-chadwm/scripts/bar.sh &
+sh ~/.config/arco-chadwm/scripts/bar.sh &
 while type chadwm >/dev/null; do chadwm && continue || break; done
