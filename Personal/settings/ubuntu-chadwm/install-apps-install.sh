2fa1bf51bba57a91a5117891192439b5476beb0d
arcolinuxz <arcolinuxinfo@gmail.com>
2024-09-20 12:26:24 +0200

    dex

--- Personal/settings/ubuntu-chadwm/install-apps-install.sh
+++ Personal/settings/ubuntu-chadwm/install-apps-install.sh
@@ -45,6 +45,7 @@ sudo apt install -y btop
 sudo apt install -y catfish
 sudo apt install -y chromium-browser
 sudo apt install -y curl
+sudo apt install -y dex
 sudo apt install -y duf
 sudo apt install -y feh
 sudo apt install -y file-roller
