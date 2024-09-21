dbd2a7091dc29c1190d7bce75a6a1c9be49d8e61
Erik Dubois <erik.dubois@gmail.com>
2024-09-21 11:34:33 +0200

    nothing to do

--- Personal/settings/mint-chadwm/install-apps-local.sh
+++ Personal/settings/mint-chadwm/install-apps-local.sh
@@ -39,13 +39,7 @@ echo "################################################################"
 tput sgr0
 echo
 
-#if you want to build it you need debhelper and gettext
-#rest is on the github of mintstick or use our script or our package
-#sudo apt install -y ./packages/mintstick_1.6.2_all.deb
-sudo apt install -y mintstick
-
-#sudo apt install -y ./packages/bat_0.24.0_amd64.deb
-sudo apt install -y bat
+echo "Nothing to do"
 
 echo
 tput setaf 6
