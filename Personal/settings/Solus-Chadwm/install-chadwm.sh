34f5739f701335d2a09a269c50a8e390a5ddfeb0
Erik Dubois <erik.dubois@gmail.com>
2024-09-27 10:26:15 +0200

    solus and chadwm

---
+++ Personal/settings/Solus-Chadwm/install-chadwm.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# set -e
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
+
+##################################################################################################################
+
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### Installing Chadwm on Ubuntu 24.04"
+echo "################################################################"
+tput sgr0
+echo
+
+echo
+tput setaf 2
+echo "################################################################"
+echo "###### Installing packages"
+echo "################################################################"
+tput sgr0
+echo
+
+# getting dependencies to be able to build Chadwm
+sudo eopkg install -y -c system.devel
+sudo eopkg install -y font-awesome-ttf
+sudo eopkg install -y imlib2-devel
+sudo eopkg install -y libx11-devel
+sudo eopkg install -y libxft-devel
+sudo eopkg install -y libxinerama-devel
+
+# applications to be used in Chadwm
+sudo eopkg install -y alacritty
+sudo eopkg install -y picom
+sudo eopkg install -y polkit-gnome
+sudo eopkg install -y rofi
+sudo eopkg install -y sxhkd
+sudo eopkg install -y dmenu
+sudo eopkg install -y thunar
+sudo eopkg install -y thunar-archive-plugin
+sudo eopkg install -y thunar-volman
+
+# exit strategy - super + shift + x
+git clone https://github.com/arcolinux/arcolinux-powermenu  /tmp/arcolinux-powermenu
+sudo cp /tmp/arcolinux-powermenu/usr/local/bin/arcolinux-powermenu /usr/local/bin
+cp -r /tmp/arcolinux-powermenu/etc/skel/.bin ~
+cp -r /tmp/arcolinux-powermenu/etc/skel/.config ~
+
+# getting the official code from ArcoLinux
+git clone https://github.com/arcolinux/arcolinux-chadwm  /tmp/arcolinux-chadwm
+sudo cp /tmp/arcolinux-chadwm/usr/bin/exec-chadwm /usr/bin
+sudo cp /tmp/arcolinux-chadwm/usr/share/xsessions/chadwm.desktop /usr/share/xsessions
+cp -r /tmp/arcolinux-chadwm/etc/skel/.bin ~
+cp -r /tmp/arcolinux-chadwm/etc/skel/.config ~
+
+# overwriting the official code from ArcoLinux with my own
+cp run.sh  ~/.config/arco-chadwm/scripts
+cp picom.conf  ~/.config/arco-chadwm/picom
+cp config.def.h ~/.config/arco-chadwm/chadwm
+cp sxhkdrc  ~/.config/arco-chadwm/sxhkd
+cp bar.sh ~/.config/arco-chadwm/scripts
+[ -d $HOME"/.config/Thunar" ] || mkdir -p $HOME"/.config/Thunar"
+cp uca.xml ~/.config/Thunar/
+
+# building Chadwm
+cd ~/.config/arco-chadwm/chadwm
+sudo make install
+
+# removing this package - it slows down terminals and thunar
+sudo eopkg remove -y xdg-desktop-portal-gnome
+
+echo
+tput setaf 6
+echo "################################################################"
+echo "###### Chadwm is installed - reboot"
+echo "################################################################"
+tput sgr0
+echo
