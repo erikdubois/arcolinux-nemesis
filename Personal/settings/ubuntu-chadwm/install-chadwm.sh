ebdde61882890c0ce4d8864cd0c1bc40d9ebfdb0
Erik Dubois <erik.dubois@gmail.com>
2024-09-28 13:05:53 +0200

    pulsemixer

--- Personal/settings/ubuntu-chadwm/install-chadwm.sh
+++ Personal/settings/ubuntu-chadwm/install-chadwm.sh
@@ -60,6 +60,7 @@ sudo apt install -y alacritty
 sudo apt install -y picom
 sudo apt install -y playerctl
 sudo apt install -y policykit-1-gnome
+sudo apt install -y pulsemixer
 sudo apt install -y rofi
 sudo apt install -y sxhkd
 # next item is dmenu
