3927767da80421ebc67c45feb011a9baad420639
Erik Dubois <erik.dubois@gmail.com>
2024-09-30 06:50:05 +0200

    missing t

--- Personal/settings/ubuntu-chadwm/install-apps-ppa.sh
+++ Personal/settings/ubuntu-chadwm/install-apps-ppa.sh
@@ -41,7 +41,7 @@ echo
 
 # https://www.sublimetext.com/docs/linux_repositories.html
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.lis
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install sublime-text
 
