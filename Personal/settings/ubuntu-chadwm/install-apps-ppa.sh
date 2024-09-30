8c77b0547deb5991e77bd0b83e07486998f2df48
Erik Dubois <erik.dubois@gmail.com>
2024-09-30 06:48:32 +0200

    sublime

--- Personal/settings/ubuntu-chadwm/install-apps-ppa.sh
+++ Personal/settings/ubuntu-chadwm/install-apps-ppa.sh
@@ -39,8 +39,14 @@ echo "################################################################"
 tput sgr0
 echo
 
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
+# https://www.sublimetext.com/docs/linux_repositories.html
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.lis
+sudo apt update
+sudo apt install sublime-text
 
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
+sudo apt update
 sudo apt install -y fastfetch
 
 #https://www.spotify.com/cw-nl/download/linux/
