27f5c8d08e1f0344666c531a230dc6b3c663e387
Erik Dubois <erik.dubois@gmail.com>
2024-10-12 11:46:04 +0200

    space

--- Personal/settings/ubuntu-chadwm/install-apps-ppa.sh
+++ Personal/settings/ubuntu-chadwm/install-apps-ppa.sh
@@ -170,7 +170,7 @@ echo "################################################################"
 echo
 
 wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list > /dev/null' 
+sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list > /dev/null'
 sudo apt update
 sudo apt install opera-stable -y
 
