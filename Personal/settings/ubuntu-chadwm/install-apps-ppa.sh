1f50eb0e51fe397ad0b0c15187298866d66c5f02
Erik Dubois <erik.dubois@gmail.com>
2024-10-12 13:12:27 +0200

    opera new ppa

--- Personal/settings/ubuntu-chadwm/install-apps-ppa.sh
+++ Personal/settings/ubuntu-chadwm/install-apps-ppa.sh
@@ -168,9 +168,9 @@ echo "################################################################"
 echo "###### Opera"
 echo "################################################################"
 echo
-
-wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" | sudo tee /etc/apt/sources.list.d/opera-stable.list > /dev/null'
+# https://deb.opera.com/manual.html
+wget -qO- https://deb.opera.com/archive.key | gpg --dearmor | sudo dd of=/usr/share/keyrings/opera-browser.gpg
+echo "deb [signed-by=/usr/share/keyrings/opera-browser.gpg] https://deb.opera.com/opera-stable/ stable non-free" | sudo dd of=/etc/apt/sources.list.d/opera-archive.list
 sudo apt update
 sudo apt install opera-stable -y
 
