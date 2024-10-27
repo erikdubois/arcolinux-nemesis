8cd5ca07330ff00076d34786293e50a44e68cd30
Erik Dubois <erik.dubois@gmail.com>
2024-10-27 10:23:43 +0100

    arch=amd64

--- Personal/settings/mint-chadwm/install-apps-ppa.sh
+++ Personal/settings/mint-chadwm/install-apps-ppa.sh
@@ -97,7 +97,7 @@ sudo apt install curl -y
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 sudo apt update
 
