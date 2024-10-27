62366a76aecc996c5eee06fc078a306f331978bb
Erik Dubois <erik.dubois@gmail.com>
2024-10-27 10:25:46 +0100

    text

--- Personal/settings/mint-chadwm/1-all-in-one.sh
+++ Personal/settings/mint-chadwm/1-all-in-one.sh
@@ -34,7 +34,7 @@ installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 echo
 tput setaf 2
 echo "################################################################"
-echo "################### All in one"
+echo "################### All in one for Mint"
 echo "################################################################"
 tput sgr0
 echo
