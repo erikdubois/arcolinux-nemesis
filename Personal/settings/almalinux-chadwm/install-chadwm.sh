52fca5dc9166d99fed17a5314ff56973930e3f76
Erik Dubois <erik.dubois@gmail.com>
2024-11-08 10:00:37 +0100

    update

--- Personal/settings/almalinux-chadwm/install-chadwm.sh
+++ Personal/settings/almalinux-chadwm/install-chadwm.sh
@@ -129,8 +129,7 @@ if [ ! -f /usr/local/bin/sxhkd ]; then
 	# sudo yum install -y sxhkd
 
 	# Install dependencies
-	sudo dnf install -y gcc make libX11-devel libxcb-devel xcb-util-keysyms xcb-util-wm
-
+    sudo dnf install -y gcc make xorg-x11-server-devel libX11-devel libxcb-devel xcb-util-keysyms-devel
 	# Check if /tmp/sxhkd exists and remove if it does
 	if [ -d "/tmp/sxhkd" ]; then
 	    echo "Directory /tmp/sxhkd exists. Removing it..."
