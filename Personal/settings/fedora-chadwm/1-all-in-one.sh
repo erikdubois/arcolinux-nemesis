36775e15537ba5b337ba57929465e30a00156595
Erik Dubois <erik.dubois@gmail.com>
2024-11-16 07:24:25 +0100

    fedora

--- Personal/settings/fedora-chadwm/1-all-in-one.sh
+++ Personal/settings/fedora-chadwm/1-all-in-one.sh
@@ -47,10 +47,6 @@ sudo dnf update -y
 sudo dnf upgrade -y
 sudo dnf autoremove -y
 
-# building Chadwm
-cd ~/.config/arco-chadwm/chadwm
-sudo make install
-
 echo
 tput setaf 6
 echo "################################################################"
