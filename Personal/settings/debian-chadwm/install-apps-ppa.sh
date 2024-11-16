5f8e6e2f3224785da8fe39a099f7aa699661841d
Erik Dubois <erik.dubois@gmail.com>
2024-11-16 09:35:21 +0100

    -y

--- Personal/settings/debian-chadwm/install-apps-ppa.sh
+++ Personal/settings/debian-chadwm/install-apps-ppa.sh
@@ -85,7 +85,7 @@ echo
 
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
 echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
-sudo apt update && sudo apt install vivaldi-stable
+sudo apt update && sudo apt install vivaldi-stable -y
 
 echo
 echo "################################################################"
