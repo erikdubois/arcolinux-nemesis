5d72981f9e559a09905af55310d39486643cbaaa
Erik Dubois <erik.dubois@gmail.com>
2024-11-17 10:35:50 +0100

    vga

--- Personal/settings/anduin-chadwm/run.sh
+++ Personal/settings/anduin-chadwm/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 setxkbmap be 
 
-xrandr --output VGA-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output Virtual1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
 sxhkd -c ~/.config/arco-chadwm/sxhkd/sxhkdrc &
--- Personal/settings/freebsd-chadwm/run.sh
+++ Personal/settings/freebsd-chadwm/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xrandr --output Virtual1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output VGA-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
 sxhkd -c ~/.config/arco-chadwm/sxhkd/sxhkdrc &
