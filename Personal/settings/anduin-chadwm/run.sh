cf0674a9e18876fabb84e714ee0d6a6a882b89a9
Erik Dubois <erik.dubois@gmail.com>
2024-11-17 10:24:19 +0100

    keymap

--- Personal/settings/anduin-chadwm/run.sh
+++ Personal/settings/anduin-chadwm/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+setxkbmap be 
 
 xrandr --output Virtual1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
