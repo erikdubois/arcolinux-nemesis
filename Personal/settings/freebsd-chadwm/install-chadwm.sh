5cccd25f5f80eaf5d25ac87e75a0a66e75e99bd1
Erik Dubois <erik.dubois@gmail.com>
2024-11-17 10:26:41 +0100

    python3

--- Personal/settings/freebsd-chadwm/install-chadwm.sh
+++ Personal/settings/freebsd-chadwm/install-chadwm.sh
@@ -61,6 +61,7 @@ sudo pkg install -y alacritty
 sudo pkg install -y dmenu
 sudo pkg install -y picom
 sudo pkg install -y playerctl
+sudo pkg install -y python3
 #sudo pkg install -y policykit-1-gnome
 #sudo pkg install -y pulsemixer
 sudo pkg install -y rofi
