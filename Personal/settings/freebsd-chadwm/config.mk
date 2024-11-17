a40466db32f980c30578241c13fd99c25c49fcc0
Erik Dubois <erik.dubois@gmail.com>
2024-11-17 09:25:00 +0100

    config.mk

--- Personal/settings/freebsd-chadwm/config.mk
+++ Personal/settings/freebsd-chadwm/config.mk
@@ -3,15 +3,11 @@ VERSION = 6.5
 
 # Customize below to fit your system
 
-# paths
-PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
-
 # X11 include directory
-X11INC = /usr/include/X11
+X11INC = /usr/local/include/X11
 
 # X11 library directory
-X11LIB = /usr/lib/X11
+X11LIB = /usr/local/lib/X11
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -19,11 +15,9 @@ XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/include/freetype2
+FREETYPEINC = /usr/local/include/freetype2
 
-# OpenBSD (uncomment)
-FREETYPEINC = ${X11INC}/freetype2
-MANPREFIX = ${PREFIX}/man
+MANPREFIX = /usr/local/share/man
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
