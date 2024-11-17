2e5352240d5153f3db746f2ebcb0c7ab81017004
Erik Dubois <erik.dubois@gmail.com>
2024-11-17 09:16:33 +0100

    update

--- Personal/settings/freebsd-chadwm/config.mk
+++ Personal/settings/freebsd-chadwm/config.mk
@@ -21,6 +21,10 @@ XINERAMAFLAGS = -DXINERAMA
 FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
 
+# OpenBSD (uncomment)
+FREETYPEINC = ${X11INC}/freetype2
+MANPREFIX = ${PREFIX}/man
+
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender -lImlib2 -lXext
