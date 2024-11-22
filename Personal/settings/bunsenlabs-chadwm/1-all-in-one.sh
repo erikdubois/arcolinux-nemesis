c59834bc99c206449b29183dcc654fcb2a0d1cb3
Erik Dubois <erik.dubois@gmail.com>
2024-11-22 21:20:35 +0100

    name change

--- Personal/settings/bunsenlabs-chadwm/1-all-in-one.sh
+++ Personal/settings/bunsenlabs-chadwm/1-all-in-one.sh
@@ -34,7 +34,7 @@ installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 echo
 tput setaf 2
 echo "################################################################"
-echo "################### All in one for Debian"
+echo "################### All in one for Bunsenlabs"
 echo "################################################################"
 tput sgr0
 echo
