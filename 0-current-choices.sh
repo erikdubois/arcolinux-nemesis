de5ef1d8dcd49c893d7b3f3cb24e10ffbe94e525
Erik Dubois <erik.dubois@gmail.com>
2024-11-25 12:06:02 +0100

    update

--- 0-current-choices.sh
+++ 0-current-choices.sh
@@ -105,9 +105,9 @@ if grep -q 'arcolinux_repo_xlarge' /etc/pacman.conf && \
   echo "################################################################"
   tput sgr0
   echo
-  sed -i '/# \[arcolinux_repo_xlarge\]/s/^# //' /etc/pacman.conf
-  sed -i '/#SigLevel = PackageRequired DatabaseNever/s/^# //' /etc/pacman.conf
-  sed -i '/#Include = \/etc\/pacman.d\/arcolinux-mirrorlist/s/^# //' /etc/pacman.conf
+  sudo sed -i '/# \[arcolinux_repo_xlarge\]/s/^# //' /etc/pacman.conf
+  sudo sed -i '/#SigLevel = PackageRequired DatabaseNever/s/^# //' /etc/pacman.conf
+  sudo sed -i '/#Include = \/etc\/pacman.d\/arcolinux-mirrorlist/s/^# //' /etc/pacman.conf
 
   else
   echo
