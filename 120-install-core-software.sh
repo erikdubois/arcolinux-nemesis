a92c294c3d6905ba4c2de1282cf9287397338c2a
arcolinuxz <arcolinuxinfo@gmail.com>
2025-01-11 07:37:32 +0100

    mission-center

--- 120-install-core-software.sh
+++ 120-install-core-software.sh
@@ -151,6 +151,7 @@ sudo pacman -S --noconfirm --needed lollypop
 sudo pacman -S --noconfirm --needed lshw
 sudo pacman -S --noconfirm --needed man-db
 sudo pacman -S --noconfirm --needed man-pages
+sudo pacman -S --noconfirm --needed mission-center
 sudo pacman -S --noconfirm --needed mkinitcpio-firmware
 sudo pacman -S --noconfirm --needed mlocate
 sudo pacman -S --noconfirm --needed meld
