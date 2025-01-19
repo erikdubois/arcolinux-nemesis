89770b2b6f0445242b6f68d0a03b9a01fe27a5e8
arcolinuxz <arcolinuxinfo@gmail.com>
2025-01-19 12:16:32 +0100

    mirrors

--- 120-install-core-software.sh
+++ 120-install-core-software.sh
@@ -47,12 +47,14 @@ if ! grep -q -e "Manjaro" -e "Artix" /etc/os-release; then
 
   echo "Deleting current /etc/pacman.d/mirrorlist and replacing with"
   echo
-  echo "Server = https://mirror.osbeck.com/archlinux/\$repo/os/\$arch
-Server = http://mirror.osbeck.com/archlinux/\$repo/os/\$arch
+echo "## Best Arch Linux servers worldwide from arcolinux-nemesis-120
+
 Server = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch
+Server = https://mirrors.kernel.org/archlinux/\$repo/os/\$arch
 Server = http://mirror.rackspace.com/archlinux/\$repo/os/\$arch
 Server = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch
-Server = https://mirrors.kernel.org/archlinux/\$repo/os/\$arch" | sudo tee /etc/pacman.d/mirrorlist
+Server = https://mirror.osbeck.com/archlinux/\$repo/os/\$arch
+Server = http://mirror.osbeck.com/archlinux/\$repo/os/\$arch"  | sudo tee /etc/pacman.d/mirrorlist
   echo
 fi
 
