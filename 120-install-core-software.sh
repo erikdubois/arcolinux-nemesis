2bc383d780ea51e54a1a077443ad3ef9ca3b56bf
Erik Dubois <erik.dubois@gmail.com>
2025-04-14 09:46:29 +0200

    bibata

--- 120-install-core-software.sh
+++ 120-install-core-software.sh
@@ -109,7 +109,7 @@ sudo pacman -S --noconfirm --needed avahi
 sudo pacman -S --noconfirm --needed baobab
 sudo pacman -S --noconfirm --needed bash-completion
 sudo pacman -S --noconfirm --needed bat
-sudo pacman -S --noconfirm --needed bibata-cursor-theme-bin
+sudo pacman -S --noconfirm --needed bibata-cursor-theme
 sudo pacman -S --noconfirm --needed btop
 sudo pacman -S --noconfirm --needed chromium
 sudo pacman -S --noconfirm --needed cpuid
