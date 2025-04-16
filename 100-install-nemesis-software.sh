cc130a1602ebd79cf7c5a7a33e1315c011a481ed
Erik Dubois <erik.dubois@gmail.com>
2025-04-16 10:57:54 +0200

    no git

--- 100-install-nemesis-software.sh
+++ 100-install-nemesis-software.sh
@@ -96,7 +96,7 @@ sudo pacman -S --noconfirm --needed hardcode-fixer-git
 sudo pacman -S --noconfirm --needed lastpass
 sudo pacman -S --noconfirm --needed neo-candy-icons-git
 sudo pacman -S --noconfirm --needed pamac-aur
-sudo pacman -S --noconfirm --needed rofi-lbonn-wayland-git
+sudo pacman -S --noconfirm --needed rofi-lbonn-wayland
 sudo pacman -S --noconfirm --needed sparklines-git
 sudo pacman -S --noconfirm --needed surfn-icons-git
 sudo pacman -S --noconfirm --needed wttr
