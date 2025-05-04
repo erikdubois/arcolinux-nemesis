75d3ec1aaadd16393f68b01c507992441a8e2c38
byluratgit <zauggmarcel@outlook.de>
2025-05-04 15:58:29 +0200

    Add files via upload
    remove insync

--- 120-install-core-software.sh
+++ 120-install-core-software.sh
@@ -112,7 +112,7 @@ sudo pacman -S --noconfirm --needed hddtemp
 sudo pacman -S --noconfirm --needed hw-probe
 sudo pacman -S --noconfirm --needed hyfetch
 sudo pacman -S --noconfirm --needed inkscape
-sudo pacman -S --noconfirm --needed insync
+#sudo pacman -S --noconfirm --needed insync
 sudo pacman -S --noconfirm --needed linux-firmware-qlogic
 sudo pacman -S --noconfirm --needed logrotate
 sudo pacman -S --noconfirm --needed lolcat
