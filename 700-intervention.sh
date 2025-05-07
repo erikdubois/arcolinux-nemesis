93078185dd86fe184d1ee37db525d58839d691cd
Erik Dubois <erik.dubois@gmail.com>
2025-05-07 10:52:09 +0200

    cachyos

--- 700-intervention.sh
+++ 700-intervention.sh
@@ -43,6 +43,38 @@ fi
 
 ##################################################################################################################################
 
+if grep -q "CachyOS" /etc/os-release; then
+
+  echo
+  tput setaf 2
+  echo "########################################################################"
+  echo "################### We are on Manjaro"
+  echo "########################################################################"
+  tput sgr0
+  echo
+
+  # putting back the original pacman.conf
+  sudo cp -v /etc/pacman.conf.nemesis /etc/pacman.conf
+
+# Define the lines to append
+config_block="
+[nemesis_repo]
+SigLevel = Never
+Server = https://erikdubois.github.io/\$repo/\$arch
+
+[chaotic-aur]
+SigLevel = Required DatabaseOptional
+Include = /etc/pacman.d/chaotic-mirrorlist
+"
+
+  # Append to /etc/pacman.conf
+  echo "$config_block" | sudo tee -a /etc/pacman.conf > /dev/null
+
+  echo "Repositories (chaotic/nemesis) added to /etc/pacman.conf"
+
+fi
+
+
 if grep -q "Manjaro" /etc/os-release; then
 
   echo
