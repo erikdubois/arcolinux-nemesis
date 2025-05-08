9087e6bbf77c5593d9427261b94cea3ffd4a0d71
Erik Dubois <erik.dubois@gmail.com>
2025-05-08 12:15:31 +0200

    update

---
+++ 180-ananicy.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#set -e
+##################################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
+##################################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################################
+
+installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
+
+##################################################################################################################################
+
+if [ "$DEBUG" = true ]; then
+    echo
+    echo "------------------------------------------------------------"
+    echo "Running $(basename $0)"
+    echo "------------------------------------------------------------"
+    echo
+    read -n 1 -s -r -p "Debug mode is on. Press any key to continue..."
+    echo
+fi
+
+##################################################################################################################################
+
+echo
+tput setaf 3
+echo "########################################################################"
+echo "################### Ananicy"
+echo "########################################################################"
+tput sgr0
+echo
+
+func_install() {
+    if pacman -Qi $1 &> /dev/null; then
+        tput setaf 2
+        echo "#######################################################################################"
+        echo "################## The package "$1" is already installed"
+        echo "#######################################################################################"
+        echo
+        tput sgr0
+    else
+        tput setaf 3
+        echo "#######################################################################################"
+        echo "##################  Installing package "  $1
+        echo "#######################################################################################"
+        echo
+        tput sgr0
+        sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+
+installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
+
+sudo pacman -S --noconfirm --needed ananicy-cpp
+yay -S cachyos-ananicy-rules --noconfirm
+
+sudo systemctl enable --now ananicy-cpp.service
+
+echo
+tput setaf 6
+echo "##############################################################"
+echo "###################  $(basename $0) done"
+echo "##############################################################"
+tput sgr0
+echo
