bf030110db5161dac3355c31accecc7c7f46ed57
Erik Dubois <erik.dubois@gmail.com>
2025-05-21 09:14:23 +0200

    update

--- 100-remove-software.sh
+++ 100-remove-software.sh
@@ -184,6 +184,7 @@ if [[ -f /etc/dev-rel ]]; then
   remove_if_installed arcolinux-meta-log
   remove_if_installed arcolinux-paru-git
   remove_if_installed arcolinux-powermenu-git
+  remove_if_installed arcolinux-qt5-git
   remove_if_installed arcolinux-rofi-git
   remove_if_installed arcolinux-rofi-themes-git
   remove_if_installed arcolinux-root-git
