b13740fbf603216dbb356abce23d2515c7990762
Erik Dubois <erik.dubois@gmail.com>
2025-05-24 08:10:44 +0200

    update

--- Personal/settings/voyage-of-chadwm/mint-chadwm/run.sh
+++ Personal/settings/voyage-of-chadwm/mint-chadwm/run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Extract the correct Virtual output (either Virtual-1 or Virtual1)
+VIRTUAL_OUTPUT=$(xrandr | grep -oP '^Virtual-?1(?=\sconnected)')
+
+# If an output was found, apply xrandr settings
+if [[ -n $VIRTUAL_OUTPUT ]]; then
+    xrandr --output "$VIRTUAL_OUTPUT" --primary --mode 1920x1080 --pos 0x0 --rotate normal
+fi
+
 sxhkd -c ~/.config/arco-chadwm/sxhkd/sxhkdrc &
 
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
