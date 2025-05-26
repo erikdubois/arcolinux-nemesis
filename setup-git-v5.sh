02da142e480a000edaa2c7cf127cf79385c39114
Erik Dubois <erik.dubois@gmail.com>
2025-05-26 07:05:31 +0200

    update

--- setup-git-v5.sh
+++ setup-git-v5.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-#
+#set -e
 ##################################################################################################################################
-# Written to be used on 64 bits computers
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-##################################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Youtube   : https://youtube.com/erikdubois
 ##################################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
+##################################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
 ##################################################################################################################################
 
 # Problem solving commands
