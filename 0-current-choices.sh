25cf4918d28da86b2207633e4d97f9f7cb812e79
Erik Dubois <erik.dubois@gmail.com>
2025-05-28 07:47:31 +0200

    update

--- 0-current-choices.sh
+++ 0-current-choices.sh
@@ -133,9 +133,9 @@ if ! grep -q -e "Manjaro" -e "Artix" /etc/os-release; then
   echo
 echo "## Best Arch Linux servers worldwide from arcolinux-nemesis
 
-Server = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch
 Server = http://mirror.rackspace.com/archlinux/\$repo/os/\$arch
 Server = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch
+Server = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch
 Server = https://mirror.osbeck.com/archlinux/\$repo/os/\$arch
 Server = http://mirror.osbeck.com/archlinux/\$repo/os/\$arch
 Server = https://mirrors.kernel.org/archlinux/\$repo/os/\$arch"  | sudo tee /etc/pacman.d/mirrorlist
