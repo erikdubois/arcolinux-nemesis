c17687850add4ca7c5ddc32345c66a705a6cb29e
Erik Dubois <erik.dubois@gmail.com>
2025-05-28 07:44:03 +0200

    R

--- 100-remove-software.sh
+++ 100-remove-software.sh
@@ -44,7 +44,7 @@ remove_if_installed() {
         if [ -n "$matches" ]; then
             for pkg in $matches; do
                 echo "Removing package: $pkg"
-                sudo pacman -Rs --noconfirm "$pkg"
+                sudo pacman -R --noconfirm "$pkg"
             done
         else
             echo "No packages matching '$pattern' are installed."
