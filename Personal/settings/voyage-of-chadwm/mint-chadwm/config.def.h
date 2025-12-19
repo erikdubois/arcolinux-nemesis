f9c328811efc854fb96709baec05ccd2b1633ef4
Erik Dubois <erik.dubois@gmail.com>
2025-12-19 09:38:49 +0100

    update

--- Personal/settings/voyage-of-chadwm/mint-chadwm/config.def.h
+++ Personal/settings/voyage-of-chadwm/mint-chadwm/config.def.h
@@ -92,24 +92,24 @@ static char *tags[] = { "", "", "", "", "", "", "", "", 
 //static char *tags[] = { "Web", "Chat", "Edit", "Meld", "Vb", "Mail", "Video", "Image", "Files", "Music" };
 //static char *tags[] = {"一", "二", "三", "四", "五", "六", "七", "八", "九", "十"};
 
-static const char* firefox[] = { "firefox", NULL };
-static const char* vivaldi[] = { "vivaldi", NULL };
-static const char* brave[] = { "brave", "--password-store=basic", "%U", NULL };
-static const char* opera[] = { "opera", NULL };
-static const char* discord[] = { "discord", "open" , "discord", NULL };
-static const char* telegram[] = { "Telegram", "open" , "Telegram", NULL };
+//static const char* firefox[] = { "firefox", NULL };
+//static const char* vivaldi[] = { "vivaldi", NULL };
+//static const char* brave[] = { "brave", "--password-store=basic", "%U", NULL };
+//static const char* opera[] = { "opera", NULL };
+//static const char* discord[] = { "discord", "open" , "discord", NULL };
+//static const char* telegram[] = { "Telegram", "open" , "Telegram", NULL };
 static const char* mintstick[] = { "mintstick", "-m", "iso", NULL};
-static const char* pavucontrol[] = { "pavucontrol", NULL };
+//static const char* pavucontrol[] = { "pavucontrol", NULL };
 
 static const Launcher launchers[] = {
     /* command     name to display */
 
-    { discord,       "" },
-    { firefox,       "" },
+    //{ discord,       "" },
+    //{ firefox,       "" },
     { mintstick,     "" },
-    { pavucontrol,   "" },
-    { telegram,      "" },
-    { vivaldi,       "" },
+    //{ pavucontrol,   "" },
+    //{ telegram,      "" },
+    //{ vivaldi,       "" },
 };
 
 static const int tagschemes[] = {
