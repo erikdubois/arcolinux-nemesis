c106cb2c7b9d080b0838885a44271c14036b648d
Buildra <buildra@protonmail.com>
2026-02-01 09:23:50 +0100

    update

--- 100-remove-software.sh
+++ 100-remove-software.sh
@@ -721,8 +721,9 @@ if grep -q "Liya" /etc/os-release ; then
   remove_if_installed_deps timeshift-autosnap
   remove_if_installed_deps timeshift
   remove_if_installed_deps pika-backup
-  remove_if_installed_deps mpv
+  remove_if_installed_deps deluge-gtk
   remove_if_installed_deps celluloid
+  remove_if_installed_deps geary
 
 fi
 
