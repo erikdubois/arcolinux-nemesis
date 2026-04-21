0161e7d69c802a7f1ee82b795a66755432ea516e
Erik Dubois <erik.dubois@gmail.com>
2026-04-21 20:28:37 +0200

    update

--- personal/settings/voyage-of-chadwm/mint-chadwm/install-apps-ppa.sh
+++ personal/settings/voyage-of-chadwm/mint-chadwm/install-apps-ppa.sh
@@ -99,13 +99,10 @@ echo "###### Code"
 echo "########################################################################"
 echo
 
-sudo apt-get install wget gpg -y
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+sudo apt install software-properties-common apt-transport-https wget gpg -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 rm -f packages.microsoft.gpg
-
-sudo apt install -y apt-transport-https
 sudo apt update
 sudo apt install -y code # or code-insiders
 
