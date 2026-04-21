4b05e5a0853ffcfd31924dd53daef39b0ffd4dda
Erik Dubois <erik.dubois@gmail.com>
2026-04-21 20:34:29 +0200

    update

--- personal/settings/voyage-of-chadwm/mint-chadwm/install-apps-ppa.sh
+++ personal/settings/voyage-of-chadwm/mint-chadwm/install-apps-ppa.sh
@@ -109,6 +109,17 @@ sudo apt install -y code # or code-insiders
 
 echo
 echo "########################################################################"
+echo "###### Spotify"
+echo "########################################################################"
+echo
+
+curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install -y spotify-client
+
+
+echo
+echo "########################################################################"
 echo "###### Firefox"
 echo "########################################################################"
 echo
